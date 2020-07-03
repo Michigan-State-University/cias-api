cf87b5f4f144b75f7ca1db1730dee28314771100
Michał Pawlak <michal.pawlak@htdevelopers.com>
2020-07-03 08:48:56 +0200

    Add default settings to Question

--- app/models/question/single.rb
+++ app/models/question/single.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Question::Single < Question
+  before_validation :assign_custom_values
+
+  private
+
+  def assign_custom_values
+    settings['proceed_button'] ||= settings['proceed_button'] = true
+  end
 end
