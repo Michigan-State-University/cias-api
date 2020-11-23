b53a2a2895b1acf0d225ec26609ea701de2850b1
Michał Śnieć <michal.sniec@htdevelopers.com>
2020-11-23 13:26:22 +0100

    Refactor migration to not use active record method
    Fix rubocop issue

--- db/migrate/20201027181844_change_question_group.rb
+++ db/migrate/20201027181844_change_question_group.rb
@@ -4,16 +4,20 @@ class ChangeQuestionGroup < ActiveRecord::Migration[6.0]
   def up
     add_column :question_groups, :type, :string
     add_index :question_groups, :type
-    QuestionGroup.where(default: true).update_all(type: 'QuestionGroup::Default')
-    QuestionGroup.where(default: false).update_all(type: 'QuestionGroup::Plain')
+    execute <<-SQL.squish
+      update question_groups set "type" = 'QuestionGroup::Default' where "default" = true;
+      update question_groups set "type" = 'QuestionGroup::Plain' where "default" = false;
+    SQL
     remove_column :question_groups, :default
   end
 
   def down
     add_column :question_groups, :default, :boolean
     add_index :question_groups, :default
-    QuestionGroup.where(type: 'QuestionGroup::Default').update_all(default: true)
-    QuestionGroup.where(default: false).update_all(type: 'QuestionGroup::Plain')
+    execute <<-SQL.squish
+      update question_groups set "default" = true where "type" = 'QuestionGroup::Default';
+      update question_groups set "default" = false where "type" = 'QuestionGroup::Plain';
+    SQL
     remove_index :question_groups, :type
     remove_column :question_groups, :type
   end
