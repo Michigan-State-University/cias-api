f26654ddcec0de57e908eb99ffdfecbea3b2f1fb
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2020-12-10 08:38:50 +0100

    Fix rubocop issue

--- db/migrate/20201203224917_add_unique_constraint_for_pair_question_id_and_user_id_to_answers.rb
+++ db/migrate/20201203224917_add_unique_constraint_for_pair_question_id_and_user_id_to_answers.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 class AddUniqueConstraintForPairQuestionIdAndUserIdToAnswers < ActiveRecord::Migration[6.0]
   def change
     add_index :answers, %i[question_id user_id], unique: true
