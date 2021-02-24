90f822fb264ccef210b90dd5787467948940e6f5
Michał Śnieć <michal.sniec@htdevelopers.com>
2021-02-24 09:53:22 +0100

    Fix access

--- app/controllers/v1/user_sessions_controller.rb
+++ app/controllers/v1/user_sessions_controller.rb
@@ -2,15 +2,13 @@
 
 class V1::UserSessionsController < V1Controller
   def create
-    user_session = user_session_service.create(user_session_params)
+    user_session = UserSession.find_or_initialize_by(session_id: user_session_params['session_id'], user_id: current_v1_user.id)
+    authorize! :create, user_session
+    user_session.save!
     render json: serialized_response(user_session), status: :ok
   end
 
   private
-
-  def user_session_service
-    @user_session_service ||= V1::UserSessionService.new(current_v1_user)
-  end
 
   def user_session_params
     params.require(:user_session).permit(:session_id)
--- app/models/ability/participant.rb
+++ app/models/ability/participant.rb
@@ -9,10 +9,7 @@ class Ability::Participant < Ability::Base
   private
 
   def participant
-    can :read, Intervention, Intervention.available_for_participant(user.email)
-    can :read, Session, intervention_id: Intervention.available_for_participant(user.email)
-    can :read, QuestionGroup, session: { intervention_id: Intervention.available_for_participant(user.email) }
-    can :read, Question, question_group: { session: { intervention_id: Intervention.available_for_participant(user.email) } }
-    can :manage, Answer, question: { question_group: { session: { intervention_id: Intervention.available_for_participant(user.email) } } }
+    can :create, UserSession, session: { intervention: Intervention.available_for_participant(user.email) }
+    can :create, Answer, user_session: { user_id: user.id }
   end
 end
