cbe6990e9973d5ad37c129fd0a1a832ca24e5255
AdrianNowik <schab55@gmail.com>
2021-03-08 11:43:19 +0100

    Fix formula error for empty string (#194)

--- app/services/v1/generated_reports/generate_user_session_reports.rb
+++ app/services/v1/generated_reports/generate_user_session_reports.rb
@@ -14,6 +14,8 @@ class V1::GeneratedReports::GenerateUserSessionReports
 
     dentaku_calculator.store(**all_var_values) if all_var_values.present?
 
+    dentaku_calculator.memory.transform_values!(&:to_i)
+
     report_templates.each do |report_template|
       V1::GeneratedReports::Create.call(
         report_template,
