199a2d2d42e3996a68aece64cbee85784076081d
Wiktoria Różańska <wiktoriarozanska@Wiktoria-Rozanska.local>
2021-03-08 11:05:14 +0100

    added pagination and sorting in reports controller

--- app/controllers/v1/generated_reports_controller.rb
+++ app/controllers/v1/generated_reports_controller.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class V1::GeneratedReportsController < V1Controller
+  include Pagy::Backend
+  after_action { pagy_headers_merge(@pagy) if @pagy }
+
   def index
     authorize! :read, GeneratedReport
 
-    render json: serialized_response(generated_reports_scope)
+    set_limit
+    set_order
+    @pagy, @records = pagy(generated_reports_scope.order(created_at: @order), items: @limit)
+    render json: serialized_response(@records)
   end
 
   private
@@ -15,5 +21,14 @@ class V1::GeneratedReportsController < V1Controller
 
   def filter_params
     params.permit(:report_for)
+  end
+
+  def set_limit
+    @limit = params[:limit].present? ? params[:limit].to_i : 7
+  end
+
+  def set_order
+    @order = params[:order].present? ? params[:order] : "ASC"
+    @order = "ASC" if not ["ASC", "DESC"].include?(@order.upcase)
   end
 end
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 require 'pagy/extras/overflow'
+require 'pagy/extras/headers'
 
 Pagy::VARS[:overflow] = :empty_page
