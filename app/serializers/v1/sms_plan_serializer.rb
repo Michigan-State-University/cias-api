2f4f4c0df9988385ecffecbbdfdb37dd702570fd
d-obuchowski <darek-ziomek16@o2.pl>
2021-03-10 13:01:42 +0100

    changing format of end at of sms plans (#203)
    changing format of end at of sms plans

--- app/serializers/v1/sms_plan_serializer.rb
+++ app/serializers/v1/sms_plan_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class V1::SmsPlanSerializer < V1Serializer
-  attributes :session_id, :name, :schedule, :schedule_payload, :frequency, :end_at, :formula,
+  attributes :session_id, :name, :schedule, :schedule_payload, :frequency, :formula,
              :no_formula_text, :is_used_formula
   has_many :variants, serializer: V1::SmsPlan::VariantSerializer
+
+  attribute :end_at do |object|
+    object.end_at.strftime('%d/%m/%Y') if object.end_at.present?
+  end
 end
