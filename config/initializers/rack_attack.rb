f817d6c360e9ff09ccd1df0d3452c3900b7c2ca7
Adrian <schab55@gmail.com>
2021-03-16 15:48:13 +0100

    Change request limit to 100req/minute by IP

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -25,7 +25,7 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 60, period: 1.minute) do |req|
+  throttle('req/ip', limit: 100, period: 1.minute) do |req|
     req.ip unless req.path.starts_with?('/assets')
   end
 
