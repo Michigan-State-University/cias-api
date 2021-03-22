cb06be6161ca0704f9a004712292cba1893af5b8
Wiktoria Różańska <wiktoria.rozanska@htdevelopers.com>
2021-03-22 10:47:18 +0100

    remove chnages from other branch

--- app/models/answer/name.rb
+++ app/models/answer/name.rb
@@ -2,7 +2,7 @@
 
 class Answer::Name < Answer
   def on_answer
-    text = body_data.first.dig('value', 'phonetic_name')
+    text = body_data.try('value').try('phonetic_name') || ''
     user_session.name_audio = V1::AudioService.new(text).execute
     user_session.save!
   end
