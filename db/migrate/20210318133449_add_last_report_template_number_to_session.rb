e719a6b7018fd5133adaea25800cfc6251f52d57
Michał Śnieć <michal.sniec@htdevelopers.com>
2021-03-23 11:55:05 +0100

    Hotfix errbit issues

--- db/migrate/20210318133449_add_last_report_template_number_to_session.rb
+++ db/migrate/20210318133449_add_last_report_template_number_to_session.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLastReportTemplateNumberToSession < ActiveRecord::Migration[6.0]
   def change
     add_column :sessions, :last_report_template_number, :integer, default: 0
