ec142ffcef1e245b591201e9a032d7a5caabe0c9
Izabela Wiatrowska <67907972+IzabelaWiatrowska@users.noreply.github.com>
2021-03-29 14:56:29 +0200

    CIAS30-1055 Reports template are not copied/duplicated (#249)
    * make session able to clone report templates
    
    * update specs
    
    * make rubocop happy
    
    * add transaction
    
    * update specs

--- app/models/report_template/section.rb
+++ app/models/report_template/section.rb
@@ -4,4 +4,8 @@ class ReportTemplate::Section < ApplicationRecord
   belongs_to :report_template
   has_many :variants, class_name: 'ReportTemplate::Section::Variant', dependent: :destroy,
                       foreign_key: :report_template_section_id, inverse_of: :report_template_section
+
+  ATTR_NAMES_TO_COPY = %w[
+    formula
+  ].freeze
 end
