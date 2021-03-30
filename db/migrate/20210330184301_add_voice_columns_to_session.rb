dfc6091958703bd5e3e07adb07036ba4e7bfab31
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2021-03-30 21:14:36 +0200

    test and rubocop

--- db/migrate/20210330184301_add_voice_columns_to_session.rb
+++ db/migrate/20210330184301_add_voice_columns_to_session.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddVoiceColumnsToSession < ActiveRecord::Migration[6.0]
   def change
     add_column :sessions, :language_code, :string, default: 'en-US', null: false
