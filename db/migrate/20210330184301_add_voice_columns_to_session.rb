0c88051d82601edafd65b44775f7dd6cc534cbe4
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2021-03-30 20:46:42 +0200

    add columns to session

---
+++ db/migrate/20210330184301_add_voice_columns_to_session.rb
@@ -0,0 +1,6 @@
+class AddVoiceColumnsToSession < ActiveRecord::Migration[6.0]
+  def change
+    add_column :sessions, :language_code, :string, default: 'en-US', null: false
+    add_column :sessions, :voice_name, :string, default: 'en-US-Standard-C', null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_074521) do
+ActiveRecord::Schema.define(version: 2021_03_30_184301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -201,6 +201,8 @@ ActiveRecord::Schema.define(version: 2021_03_24_074521) do
     t.integer "report_templates_count"
     t.integer "sms_plans_count", default: 0, null: false
     t.integer "last_report_template_number", default: 0
+    t.string "language_code", default: "en-US", null: false
+    t.string "voice_name", default: "en-US-Standard-C", null: false
     t.index ["intervention_id", "name"], name: "index_sessions_on_intervention_id_and_name", using: :gin
     t.index ["intervention_id"], name: "index_sessions_on_intervention_id"
     t.index ["name"], name: "index_sessions_on_name"
