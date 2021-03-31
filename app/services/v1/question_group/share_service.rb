cf75f340009a3ba58a4ebbc05cdd8b673a0cabc6
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2021-04-01 00:02:51 +0200

    change in warning

--- app/services/v1/question_group/share_service.rb
+++ app/services/v1/question_group/share_service.rb
@@ -68,7 +68,7 @@ class V1::QuestionGroup::ShareService
   def validate_uniqueness(question)
     return unless [::Question::Name, ::Question::ParticipantReport, ::Question::ThirdParty, ::Question::Phone].member? question.class
 
-    self.warning = 'This type of question can appear only once per session' if question_type_exist_in_session(question)
+    self.warning = 'ViolationUniqueness' if question_type_exist_in_session(question)
   end
 
   def question_type_exist_in_session(question)
