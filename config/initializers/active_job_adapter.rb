3c7474f536bbeb8f6197cc3d3154aa270942cc6f
msniec <michal.sniec@gmail.com>
2021-05-26 11:54:40 +0200

    Fix v4

--- config/initializers/active_job_adapter.rb
+++ config/initializers/active_job_adapter.rb
@@ -1,22 +1,13 @@
 # frozen_string_literal: true
 
-# pool = ConnectionPool.new(size: ENV.fetch('REDIS_CONNECTIONS_POOL', 12)) do
-#   Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-# end
+pool = ConnectionPool.new(size: ENV.fetch('REDIS_CONNECTIONS_POOL', 12)) do
+  Redis.new(url: ENV['REDIS_URL'], driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+end
 
-#
-# Sidekiq.configure_server do |config|
-#   config.redis = pool
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = pool
-# end
-Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 Sidekiq.configure_server do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = pool
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = pool
 end
