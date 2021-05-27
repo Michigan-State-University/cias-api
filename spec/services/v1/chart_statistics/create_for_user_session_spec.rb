10a4367422a5c9414d9fb1dbb5cf0fb10f22e30f
msniec <michal.sniec@gmail.com>
2021-05-27 10:04:27 +0200

    Maybe fix

--- spec/services/v1/chart_statistics/create_for_user_session_spec.rb
+++ spec/services/v1/chart_statistics/create_for_user_session_spec.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe V1::ChartStatistics::CreateForUserSession do
-  subject { described_class.call(user_session) }
-
-  before_all do
-    RSpec::Mocks.with_temporary_scope do
-      allow_any_instance_of(::Question::Finish).to receive(:execute_narrator).and_return(true)
-    end
-  end
+  subject { described_class.call(@user_session) }
 
   let_it_be(:organization) { create(:organization) }
+  let_it_be(:reporting_dashboard) { create(:reporting_dashboard, organization: organization) }
+  let_it_be(:dashboard_section) { create(:dashboard_section, reporting_dashboard: reporting_dashboard) }
   let_it_be(:health_system) { create(:health_system, organization: organization) }
   let_it_be(:health_clinic) { create(:health_clinic, health_system: health_system) }
   let_it_be(:intervention) { create(:intervention, :published, organization: organization) }
-  let_it_be(:session) { create(:session, intervention: intervention) }
   let_it_be(:user) { create(:user) }
-  let_it_be(:user_session) { create(:user_session, session: session, user: user, health_clinic: health_clinic) }
-  let_it_be(:reporting_dashboard) { create(:reporting_dashboard, organization: organization) }
-  let_it_be(:dashboard_section) { create(:dashboard_section, reporting_dashboard: reporting_dashboard) }
 
   context 'when chart is published' do
     let_it_be(:formula1) do
@@ -74,28 +66,32 @@ RSpec.describe V1::ChartStatistics::CreateForUserSession do
     end
     let_it_be(:pie_chart1) do
       create(:chart, formula: formula1, dashboard_section: dashboard_section, published_at: Time.current,
-                     chart_type: Chart.chart_types[:pie_chart])
+             chart_type: Chart.chart_types[:pie_chart])
     end
     let_it_be(:pie_chart2) do
       create(:chart, formula: formula2, dashboard_section: dashboard_section, published_at: Time.current,
-                     chart_type: Chart.chart_types[:pie_chart])
+             chart_type: Chart.chart_types[:pie_chart])
     end
     let_it_be(:bar_chart1) do
       create(:chart, formula: formula3, dashboard_section: dashboard_section, published_at: Time.current,
-                     chart_type: Chart.chart_types[:bar_chart])
+             chart_type: Chart.chart_types[:bar_chart])
     end
     let_it_be(:bar_chart2) do
       create(:chart, formula: formula4, dashboard_section: dashboard_section, published_at: Time.current,
-                     chart_type: Chart.chart_types[:bar_chart])
+             chart_type: Chart.chart_types[:bar_chart])
     end
-
     context 'when user session contains all values of chart formula' do
-      let_it_be(:answer1) do
-        create(:answer_single, user_session: user_session, body: { data: [{ var: 'color', value: '1' }] })
+      before_all do
+        RSpec::Mocks.with_temporary_scope do
+          allow_any_instance_of(Question).to receive(:execute_narrator).and_return(true)
+          session = create(:session, intervention: intervention)
+          @user_session = create(:user_session, session: session, user: user, health_clinic: health_clinic)
+          @answer1 = create(:answer_single, user_session: @user_session, body: { data: [{ var: 'color', value: '1' }] })
+          @answer2 = create(:answer_single, user_session: @user_session, body: { data: [{ var: 'sport', value: '1' }] })
+        end
       end
-      let_it_be(:answer2) do
-        create(:answer_single, user_session: user_session, body: { data: [{ var: 'sport', value: '1' }] })
-      end
+      let(:answer1) { @answer1 }
+      let(:answer2) { @answer2 }
 
       it 'create chart statistics for bart charts and pie charts' do
         expect { subject }.to change(ChartStatistic, :count).by(4)
@@ -114,9 +110,15 @@ RSpec.describe V1::ChartStatistics::CreateForUserSession do
     end
 
     context "when user session doesn't contain all values of chart formula" do
-      let!(:answer1) do
-        create(:answer_single, user_session: user_session, body: { data: [{ var: 'color', value: '1' }] })
+      before_all do
+        RSpec::Mocks.with_temporary_scope do
+          allow_any_instance_of(Question).to receive(:execute_narrator).and_return(true)
+          session = create(:session, intervention: intervention)
+          @user_session = create(:user_session, session: session, user: user, health_clinic: health_clinic)
+          @answer1 = create(:answer_single, user_session: @user_session, body: { data: [{ var: 'color', value: '1' }] })
+        end
       end
+      let(:answer1) { @answer1 }
 
       it "Don't create chart statistic" do
         expect { subject }.not_to change(ChartStatistic, :count)
@@ -128,6 +130,27 @@ RSpec.describe V1::ChartStatistics::CreateForUserSession do
         create(:chart_statistic, label: 'Label1', organization: organization, health_system: health_system,
                                  health_clinic: health_clinic, chart: pie_chart1, user: user)
       end
+      let!(:chart_statistic2) do
+        create(:chart_statistic, label: 'Other', organization: organization, health_system: health_system,
+               health_clinic: health_clinic, chart: pie_chart2, user: user)
+      end
+      let!(:chart_statistic3) do
+        create(:chart_statistic, label: 'Matched', organization: organization, health_system: health_system,
+               health_clinic: health_clinic, chart: bar_chart1, user: user)
+      end
+      let!(:chart_statistic4) do
+        create(:chart_statistic, label: 'NotMatched', organization: organization, health_system: health_system,
+               health_clinic: health_clinic, chart: bar_chart2, user: user)
+      end
+      before_all do
+        RSpec::Mocks.with_temporary_scope do
+          allow_any_instance_of(Question).to receive(:execute_narrator).and_return(true)
+          session = create(:session, intervention: intervention)
+          @user_session = create(:user_session, session: session, user: user, health_clinic: health_clinic)
+          @answer1 = create(:answer_single, user_session: @user_session, body: { data: [{ var: 'color', value: '1' }] })
+          @answer2 = create(:answer_single, user_session: @user_session, body: { data: [{ var: 'sport', value: '1' }] })
+        end
+      end
 
       it "Don't create chart statistic" do
         expect { subject }.not_to change(ChartStatistic, :count)
@@ -136,7 +159,14 @@ RSpec.describe V1::ChartStatistics::CreateForUserSession do
   end
 
   context "when chart doesn't published" do
-    let!(:chart) { create(:chart, dashboard_section: dashboard_section, published_at: nil) }
+    before_all do
+      RSpec::Mocks.with_temporary_scope do
+        allow_any_instance_of(Question).to receive(:execute_narrator).and_return(true)
+        session = create(:session, intervention: intervention)
+        @user_session = create(:user_session, session: session, user: user, health_clinic: health_clinic)
+      end
+    end
+    let(:chart) { create(:chart, dashboard_section: dashboard_section, published_at: nil) }
 
     it "Don't create chart statistic" do
       expect { subject }.not_to change(ChartStatistic, :count)
--- spec/services/v1/chart_statistics/create_for_user_sessions_spec.rb
+++ spec/services/v1/chart_statistics/create_for_user_sessions_spec.rb
@@ -2,24 +2,13 @@
 
 RSpec.describe V1::ChartStatistics::CreateForUserSessions do
   subject { described_class.call(pie_chart.id) }
-
-  before_all do
-    RSpec::Mocks.with_temporary_scope do
-      allow_any_instance_of(::Question::Finish).to receive(:execute_narrator).and_return(true)
-    end
-  end
-
-
   let_it_be(:organization) { create(:organization) }
   let_it_be(:health_system) { create(:health_system, organization: organization) }
   let_it_be(:health_clinic) { create(:health_clinic, health_system: health_system) }
   let_it_be(:intervention) { create(:intervention, :published, organization: organization) }
-  let_it_be(:session) { create(:session, intervention: intervention) }
   let_it_be(:user) { create(:user) }
-  let_it_be(:user_session) { create(:user_session, session: session, user: user, health_clinic: health_clinic) }
   let_it_be(:reporting_dashboard) { create(:reporting_dashboard, organization: organization) }
   let_it_be(:dashboard_section) { create(:dashboard_section, reporting_dashboard: reporting_dashboard) }
-
   let_it_be(:formula) do
     { 'payload' => 'color + sport',
       'patterns' => [
@@ -39,12 +28,21 @@ RSpec.describe V1::ChartStatistics::CreateForUserSessions do
     create(:chart, formula: formula, dashboard_section: dashboard_section, published_at: Time.current,
            chart_type: Chart.chart_types[:pie_chart])
   end
-  let_it_be(:answer1) do
-    create(:answer_single, user_session: user_session, body: { data: [{ var: 'color', value: '1' }] })
+
+  before_all do
+    RSpec::Mocks.with_temporary_scope do
+      allow_any_instance_of(Question).to receive(:execute_narrator).and_return(true)
+
+      session = create(:session, intervention: intervention)
+      user_session = create(:user_session, session: session, user: user, health_clinic: health_clinic)
+
+      @answer1 = create(:answer_single, user_session: user_session, body: { data: [{ var: 'color', value: '1' }] })
+      @answer2 = create(:answer_single, user_session: user_session, body: { data: [{ var: 'sport', value: '1' }] })
+    end
   end
-  let_it_be(:answer2) do
-    create(:answer_single, user_session: user_session, body: { data: [{ var: 'sport', value: '1' }] })
-  end
+
+  let(:answer1) { @answer1 }
+  let(:answer2) { @answer2 }
 
   it 'create chart statistic' do
     expect { subject }.to change(ChartStatistic, :count).by(1)
