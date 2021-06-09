83fdf0e503b3c0c43d7cd30cbc997a99acb640e9
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2021-06-09 07:39:13 +0200

    change the condition to send an invitation to an organization admin

--- app/services/v1/organizations/invite_organization_admin.rb
+++ app/services/v1/organizations/invite_organization_admin.rb
@@ -11,18 +11,32 @@ class V1::Organizations::InviteOrganizationAdmin
   end
 
   def call
-    return if user_is_not_new_in_the_system?
+    return if already_in_the_organization?
+    return if user_is_not_organization_admin?
+    return if active_organization_admin?
 
-    new_user = User.invite!(email: email, roles: ['organization_admin'], organizable_id: organization.id, organizable_type: 'Organization')
-    organization.organization_admins << new_user
+    if user.blank?
+      new_user = User.invite!(email: email, roles: ['organization_admin'], organizable_id: organization.id, organizable_type: 'Organization')
+      organization.organization_admins << new_user
+    else
+      V1::Organizations::Invitations::Create.call(organization, user)
+    end
   end
 
   private
 
   attr_reader :organization, :email
 
-  def user_is_not_new_in_the_system?
-    user.present?
+  def already_in_the_organization?
+    organization.organization_admins.where(email: email).any?
+  end
+
+  def user_is_not_organization_admin?
+    user&.roles&.exclude?('organization_admin')
+  end
+
+  def active_organization_admin?
+    user&.active?
   end
 
   def user
