520aa2d647850da93632cc11a1ea0cef2c0a99c3
msniec <michal.sniec@gmail.com>
2021-06-09 13:32:02 +0200

    Fix tests

--- app/jobs/session_email_schedule_job.rb
+++ app/jobs/session_email_schedule_job.rb
@@ -3,9 +3,9 @@
 class SessionEmailScheduleJob < ApplicationJob
   queue_as :default
 
-  def perform(session_id, user_id, health_clinic_id = nil)
+  def perform(session_id, user_id, health_clinic = nil)
     session = Session.find_by(id: session_id)
     user = User.find_by(id: user_id)
-    session&.send_link_to_session(user, health_clinic_id) if user
+    session&.send_link_to_session(user, health_clinic) if user
   end
 end
