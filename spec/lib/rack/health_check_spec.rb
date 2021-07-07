13d5491fcc15257e4da8ed08c1abf0d9c99ebeb1
msniec <michal.sniec@gmail.com>
2021-07-07 13:09:13 +0200

    Add health checks

---
+++ spec/lib/rack/health_check_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HealthCheck do
+  let(:result)        { described_class.new.call({})  }
+  let(:status)        { result.first                  }
+  let(:json_response) { JSON.parse(result.last.first) }
+
+  describe '#call' do
+    context 'when all systems are operational' do
+      it 'returns 200 response' do
+        expect(status).to eq 200
+      end
+
+      it 'return system details in json response' do
+        expect(json_response).to eq 'database' => true
+      end
+    end
+
+    context 'when systems are not operational' do
+      before do
+        expect(ActiveRecord::Base.connection)
+          .to receive(:execute)
+                .and_raise(ActiveRecord::StatementInvalid)
+      end
+
+      it 'returns 200 response' do
+        expect(status).to eq 200
+      end
+
+      it 'return system details in json response' do
+        expect(json_response).to eq 'database' => false
+      end
+    end
+  end
+end
---
+++ spec/requests/health_check_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'GET /health_check' do
+  include Rack::Test::Methods
+
+  let(:app) do
+    Rack::Builder.parse_file(Rails.root.join('config.ru').to_s).first
+  end
+
+  before { get '/health_check' }
+
+  it 'returns 200 response' do
+    expect(last_response.status).to eq 200
+  end
+
+  it 'return system details in json response' do
+    expect(JSON.parse(last_response.body)).to eq 'database' => true
+  end
+end
