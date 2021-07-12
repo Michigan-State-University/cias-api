e4fa398b7cd7dc31733f4b8493e50625206f8bd0
maciej-gorski <maciej.gorski@htdevelopers.com>
2021-07-12 14:06:51 +0200

    implemented translation service with variable names exclusion

--- app/services/v1/translations/variable_exclusive_translation_service.rb
+++ app/services/v1/translations/variable_exclusive_translation_service.rb
@@ -1,15 +1,29 @@
+# frozen_string_literal: true
 class V1::Translations::VariableExclusiveTranslationService
   def initialize(translation_service)
     @translator = translation_service
   end
 
   def translate(text, src_language_name_short, dest_language_name_short)
+    return text if text.blank?
+    variable_names = extract_variable_names(text)
+    new_text = @translator.translate(text, src_language_name_short, dest_language_name_short)
+    insert_variable_names(new_text, variable_names)
 
   end
 
   private
 
-  def extract_variable_names(input_string)
+  @@VARIABLE_NAME_PLACEHOLDER_TOKEN = '%%%'
+  @@VARIABLE_NAME_PATTERN = Regexp.compile(/\.:.*?:\./)
 
+  def extract_variable_names(input_string)
+    input_string.scan(@@VARIABLE_NAME_PATTERN)
   end
+
+  def insert_variable_names(input_string, variable_names)
+    pattern = Regexp.escape(@@VARIABLE_NAME_PLACEHOLDER_TOKEN)
+    variable_names.reduce(input_string) { |original, var_name| original.sub(pattern, ".:#{var_name}:.") }
+  end
+
 end
