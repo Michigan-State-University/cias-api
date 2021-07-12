ac6b593fd25f478ef9d630f3fb5d839d1592e7c1
maciej-gorski <maciej.gorski@htdevelopers.com>
2021-07-12 13:52:44 +0200

    created class template for translation service without variables.

---
+++ app/services/v1/translations/variable_exclusive_translation_service.rb
@@ -0,0 +1,15 @@
+class V1::Translations::VariableExclusiveTranslationService
+  def initialize(translation_service)
+    @translator = translation_service
+  end
+
+  def translate(text, src_language_name_short, dest_language_name_short)
+
+  end
+
+  private
+
+  def extract_variable_names(input_string)
+
+  end
+end
