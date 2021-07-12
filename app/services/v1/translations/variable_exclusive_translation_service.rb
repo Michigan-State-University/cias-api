1f06facd65a839480e61bd43af36f3953a327d11
maciej-gorski <maciej.gorski@htdevelopers.com>
2021-07-12 14:37:28 +0200

    added missing variable names replacement before translation, implemented variable service test

--- app/services/v1/translations/variable_exclusive_translation_service.rb
+++ app/services/v1/translations/variable_exclusive_translation_service.rb
@@ -8,7 +8,8 @@ class V1::Translations::VariableExclusiveTranslationService
     return text if text.blank?
 
     variable_names = extract_variable_names(text)
-    new_text = @translator.translate(text, src_language_name_short, dest_language_name_short)
+    text_for_translation = text.gsub(@@VARIABLE_NAME_PATTERN, @@VARIABLE_NAME_PLACEHOLDER_TOKEN)
+    new_text = @translator.translate(text_for_translation, src_language_name_short, dest_language_name_short).to_s
     insert_variable_names(new_text, variable_names)
 
   end
@@ -24,7 +25,7 @@ class V1::Translations::VariableExclusiveTranslationService
 
   def insert_variable_names(input_string, variable_names)
     pattern = Regexp.escape(@@VARIABLE_NAME_PLACEHOLDER_TOKEN)
-    variable_names.reduce(input_string) { |original, var_name| original.sub(pattern, ".:#{var_name}:.") }
+    variable_names.reduce(input_string) { |original, var_name| original.sub(pattern, var_name) }
   end
 
 end
--- spec/services/v1/translations/variable_exclusive_translation_service_spec.rb
+++ spec/services/v1/translations/variable_exclusive_translation_service_spec.rb
@@ -3,14 +3,18 @@
 RSpec.describe V1::Translations::VariableExclusiveTranslationService do
   context 'translate text with variable names' do
     let(:translation_client) { V1::Google::TranslationService.new }
-    let(:variable_exclusive_translation_service) { V1::Translations::VariableExclusiveTranslationService.new(translation_client) }
+    let(:variable_exclusive_translation_service) { described_class.new(translation_client) }
     let(:source_language_name_short) { 'en' }
     let(:destination_language_name_short) { 'pl' }
     let(:input_with_variable_names) { 'Hello .:name:. How are you? Do you like your .:fruit:.' }
 
     it 'ignore variable names during translation' do
-
+      output = variable_exclusive_translation_service.translate(input_with_variable_names, source_language_name_short, destination_language_name_short)
+      expect(output).to eq({
+        'from' => source_language_name_short,
+        'to' => destination_language_name_short,
+        'text' => input_with_variable_names
+      }.to_s)
     end
-
   end
 end
