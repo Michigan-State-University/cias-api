b9d7863d70192a173ff674eb1a0d162a9632a0ad
maciej-gorski <maciej.gorski@htdevelopers.com>
2021-07-13 08:45:23 +0200

    changed class variables to class constants instead in variable exclusive translation service

--- app/services/v1/translations/variable_exclusive_translation_service.rb
+++ app/services/v1/translations/variable_exclusive_translation_service.rb
@@ -8,7 +8,7 @@ class V1::Translations::VariableExclusiveTranslationService
     return text if text.blank?
 
     variable_names = extract_variable_names(text)
-    text_for_translation = text.gsub(@@VARIABLE_NAME_PATTERN, @@VARIABLE_NAME_PLACEHOLDER_TOKEN)
+    text_for_translation = text.gsub(VARIABLE_NAME_PATTERN, VARIABLE_NAME_PLACEHOLDER_TOKEN)
     new_text = @translator.translate(text_for_translation, src_language_name_short, dest_language_name_short).to_s
     insert_variable_names(new_text, variable_names)
 
@@ -16,15 +16,15 @@ class V1::Translations::VariableExclusiveTranslationService
 
   private
 
-  @@VARIABLE_NAME_PLACEHOLDER_TOKEN = '%%%'
-  @@VARIABLE_NAME_PATTERN = Regexp.compile(/\.:[a-zA-Z0-9_]*?:\./)
+  VARIABLE_NAME_PLACEHOLDER_TOKEN = '%%%'
+  VARIABLE_NAME_PATTERN = Regexp.compile(/\.:[a-zA-Z0-9_]*?:\./)
 
   def extract_variable_names(input_string)
-    input_string.scan(@@VARIABLE_NAME_PATTERN)
+    input_string.scan(VARIABLE_NAME_PATTERN)
   end
 
   def insert_variable_names(input_string, variable_names)
-    pattern = Regexp.escape(@@VARIABLE_NAME_PLACEHOLDER_TOKEN)
+    pattern = Regexp.escape(VARIABLE_NAME_PLACEHOLDER_TOKEN)
     variable_names.reduce(input_string) { |original, var_name| original.sub(pattern, var_name) }
   end
 
