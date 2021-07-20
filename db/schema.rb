4cd648530b7a8ecc853fa6a714456e23ceb99d89
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2021-07-20 12:46:30 +0200

    correct offenses from rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_073541) do
+ActiveRecord::Schema.define(version: 2021_07_19_120417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
-  enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -98,8 +97,8 @@ ActiveRecord::Schema.define(version: 2021_07_14_073541) do
     t.uuid "reporting_dashboard_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "reporting_dashboard_id"], name: "index_dashboard_sections_on_name_and_reporting_dashboard_id", unique: true
     t.integer "position", default: 1, null: false
+    t.index ["name", "reporting_dashboard_id"], name: "index_dashboard_sections_on_name_and_reporting_dashboard_id", unique: true
     t.index ["reporting_dashboard_id"], name: "index_dashboard_sections_on_reporting_dashboard_id"
   end
 
@@ -189,9 +188,9 @@ ActiveRecord::Schema.define(version: 2021_07_14_073541) do
     t.uuid "organization_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "organization_id"], name: "index_health_systems_on_name_and_organization_id", unique: true
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_health_systems_on_deleted_at"
+    t.index ["name", "organization_id"], name: "index_health_systems_on_name_and_organization_id", unique: true
     t.index ["organization_id"], name: "index_health_systems_on_organization_id"
   end
 
@@ -206,7 +205,6 @@ ActiveRecord::Schema.define(version: 2021_07_14_073541) do
     t.uuid "organization_id"
     t.bigint "google_language_id", default: 22, null: false
     t.boolean "from_deleted_organization", default: false, null: false
-    t.jsonb "original_text"
     t.index ["google_language_id"], name: "index_interventions_on_google_language_id"
     t.index ["name", "user_id"], name: "index_interventions_on_name_and_user_id", using: :gin
     t.index ["name"], name: "index_interventions_on_name"
