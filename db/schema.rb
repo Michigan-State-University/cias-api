36304686bbdd2f0aefb9d6fd326fcc84131b5739
Izabela Wiatrowska <izabela.wiatrowska@htdevelopers.com>
2021-07-28 10:26:36 +0200

    fix rails migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_073541) do
+ActiveRecord::Schema.define(version: 2021_07_19_120417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
+  enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -205,7 +206,6 @@ ActiveRecord::Schema.define(version: 2021_07_14_073541) do
     t.uuid "organization_id"
     t.bigint "google_language_id", default: 22, null: false
     t.boolean "from_deleted_organization", default: false, null: false
-    t.jsonb "original_text"
     t.index ["google_language_id"], name: "index_interventions_on_google_language_id"
     t.index ["name", "user_id"], name: "index_interventions_on_name_and_user_id", using: :gin
     t.index ["name"], name: "index_interventions_on_name"
