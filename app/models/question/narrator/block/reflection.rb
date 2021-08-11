f98fbea5193f8a7544408008eb953050398102ea
Maciej Górski <86961379+maciej-gorski@users.noreply.github.com>
2021-08-11 10:27:17 +0200

    Cias1586 refactor narrator model (#492)
    * refactored reflection and reflection formula slightly
    
    * refactored reflection blocks and made build method in block.rb return block by default
    
    * added frozen_string_literal comment in reflection_base.rb
    
    * reverted the changes in narrator blocks and default build method implementation, refactored some of the narrator util classes
    
    * added comments to narrator.rb, made rubocop happy, refactored blobs.rb
    
    * removed comment
    
    * changed execute methods to call to bring it in line with the rest of services
    
    * merged changes from post-mvp-2 with narrator reflection refactor

--- app/models/question/narrator/block/reflection.rb
+++ app/models/question/narrator/block/reflection.rb
@@ -1,21 +1,4 @@
 # frozen_string_literal: true
 
-class Question::Narrator::Block::Reflection < Question::Narrator::Block
-  def build
-    block['reflections'].each_with_index do |reflection, reflection_index|
-      block['reflections'][reflection_index]['audio_urls'] =
-        Speech.new(self, index_processing, reflection).build['audio_urls']
-    end
-
-    block
-  end
-
-  def self.swap_name(block, mp3url, name_text)
-    block['reflections'].each do |reflection|
-      next reflection unless reflection['text'].include?(':name:.')
-
-      swap_name_into_block(reflection, mp3url, name_text)
-    end
-    block
-  end
+class Question::Narrator::Block::Reflection < Question::Narrator::Block::ReflectionBase
 end
