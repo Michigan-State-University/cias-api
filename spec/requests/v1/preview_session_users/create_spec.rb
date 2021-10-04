75102887e8daee6316f85b5abc45fde167509f8b
Maciej Górski <86961379+maciej-gorski@users.noreply.github.com>
2021-10-04 15:00:10 +0200

    Cias1840 cat mh preview validation (#601)
    * added validation to prevent from creating preview for CAT-MH sessions
    
    * added cat-mh preview specs

--- spec/requests/v1/preview_session_users/create_spec.rb
+++ spec/requests/v1/preview_session_users/create_spec.rb
@@ -13,14 +13,30 @@ RSpec.describe 'POST /v1/preview_session_users', type: :request do
     }
   end
   let(:preview_session_user) { User.limit_to_roles('preview_session').last }
+  let(:request) { post v1_preview_session_users_path, params: params, headers: headers }
 
   it 'set proper response headers and return created status' do
-    post v1_preview_session_users_path, params: params, headers: headers
+    request
 
     expect(response).to have_http_status(:ok)
     expect(json_response['Client']).to be_present
     expect(json_response['Access-Token']).to be_present
     expect(json_response['Uid']).to eq preview_session_user.uid
     expect(preview_session_user.preview_session_id).to eq session.id
+  end
+
+  context 'CAT-MH preview' do
+    let(:session) { create(:cat_mh_session, intervention: intervention) }
+    let(:params) { { session_id: session.id } }
+
+    before { request }
+
+    it 'returns correct HTTP status code (Method Not Allowed)' do
+      expect(response).to have_http_status(:method_not_allowed)
+    end
+
+    it 'returns proper message' do
+      expect(json_response['message']).to eq I18n.t('users.preview.cat_mh')
+    end
   end
 end
