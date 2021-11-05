cb7c1d45c250aa0fced2a99cd38d3882eb1254ba
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-11-05 11:17:34 +0100

    Use good_job async mode in production environment (#659)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.good_job.execution_mode = :async
+
+  # Or with more configuration
+  config.good_job = {
+    execution_mode: :async,
+    max_threads: 4,
+    poll_interval: 30
+  }
 end
