2561d3f48fef0d66af838858019e4e0d47c44a50
maciej-gorski <maciej.gorski@htdevelopers.com>
2022-01-17 09:56:20 +0100

    added before_save callback

--- app/models/question/tlfb.rb
+++ app/models/question/tlfb.rb
@@ -2,8 +2,15 @@
 
 class Question::Tlfb < Question
   validate :tlfb_validation
+  before_save :assign_tlfb_defaults
 
   protected
 
   def tlfb_validation; end
+
+  def assign_tlfb_defaults
+    self.subtitle = false
+    self.title = false
+    settings['proceed_button'] = false
+  end
 end
