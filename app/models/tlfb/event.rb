3b2a7586301c3f057f45ed6074516ef38c362cd1
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2022-02-07 09:06:05 +0100

    remove recursion (#726)

--- app/models/tlfb/event.rb
+++ app/models/tlfb/event.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Tlfb::Event < ApplicationRecord
-  belongs_to :day, class_name: 'Tlfb::Day', dependent: :destroy
+  belongs_to :day, class_name: 'Tlfb::Day'
 end
