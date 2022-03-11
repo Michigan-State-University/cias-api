55ef35882aa7d0b4677b22749ee9e2d15a812b91
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2022-03-11 09:59:00 +0100

    [Cias30-2190] populate intervention sessions count (#745)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_133739) do
+ActiveRecord::Schema.define(version: 2022_03_09_142828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
