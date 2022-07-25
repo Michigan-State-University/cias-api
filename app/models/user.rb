4a6c901b250c585eb973304a499b87dc80794804
Mateusz Miś <mateusz.mis@htdevelopers.com>
2022-07-25 10:58:22 +0200

    Add comment to user model for better visability

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,8 @@ class User < ApplicationRecord
   # REPORTS AVAILABLE FOR THIRD PARTY USER
   has_many :generated_reports_third_party_users, foreign_key: :third_party_id, inverse_of: :third_party,
                                                  dependent: :destroy
+
+  # DOWNLOADED REPORTS AVAILABLE FOR USER
   has_many :downloaded_reports, dependent: :destroy
   has_many :generated_reports, through: :downloaded_reports
 
