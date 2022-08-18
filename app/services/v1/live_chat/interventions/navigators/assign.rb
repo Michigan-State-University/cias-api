f95082851ce28697c179e2d692f35f8e69e89249
msniec <45016936+msniec@users.noreply.github.com>
2022-08-18 01:21:28 -0700

    [CIAS-2670] - Fix navigator from team email send (#893)

--- app/services/v1/live_chat/interventions/navigators/assign.rb
+++ app/services/v1/live_chat/interventions/navigators/assign.rb
@@ -14,8 +14,7 @@ class V1::LiveChat::Interventions::Navigators::Assign
     navigator = intervention.navigators_from_team.find(user_id)
     intervention.navigators << navigator
 
-    LiveChat::NavigatorMailer.navigator_from_team_invitation(navigator.email, intervention)
-
+    LiveChat::NavigatorMailer.navigator_from_team_invitation(navigator.email, intervention).deliver_now
     navigator
   end
 
