d3790e9de0341e26f42b45491e3964e07643cff6
msniec <45016936+msniec@users.noreply.github.com>
2022-08-22 10:08:23 -0700

    [CIAS-2669] - Invite same navigator to an intervention fix (#891)
    * [CIAS-2669] - Invite same navigator to an intervention fix
    
    * [CIAS-2669] - Make rubocop happy
    
    * [CIAS-2669] - Code review changes
    
    * [CIAS-2669] - Code review changes
    
    * [CIAS-2669] - Make rubocop happy

--- app/services/v1/live_chat/interventions/navigators/assign.rb
+++ app/services/v1/live_chat/interventions/navigators/assign.rb
@@ -11,7 +11,7 @@ class V1::LiveChat::Interventions::Navigators::Assign
   end
 
   def call
-    navigator = intervention.navigators_from_team.find(user_id)
+    navigator = intervention.available_navigator_from_team.find(user_id)
     intervention.navigators << navigator
 
     LiveChat::NavigatorMailer.navigator_from_team_invitation(navigator.email, intervention).deliver_now
