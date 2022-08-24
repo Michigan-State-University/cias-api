aab4be4499668c2e46c0ea4c9c882a0216f7844c
Mateusz Miś <mateusz.mis@htdevelopers.com>
2022-08-24 16:52:37 +0200

    Migration data type fix

--- db/migrate/20220824134311_create_notifications.rb
+++ db/migrate/20220824134311_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :live_chat_notifications do |t|
       t.references :object, polymorphic: true, null: false
-      t.datetime :timestamp
+      t.timestamp :timestamp
       t.string :type
       t.boolean :isRead
       t.string :optional_link
