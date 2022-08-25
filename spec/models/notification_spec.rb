7d52b8aa3896e8198f42c7f0283d543b86ee0f1c
Mateusz Miś <mateusz.mis@htdevelopers.com>
2022-08-25 16:02:09 +0200

    Fix model test

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe LiveChat::Notification, type: :model do
+RSpec.describe Notification, type: :model do
   it { should belong_to(:notifiable) }
   it { should belong_to(:user) }
 end
