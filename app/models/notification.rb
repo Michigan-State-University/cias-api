292e35970e539d8790e95698e0febd0df984a8d2
Mateusz Miś <mateusz.mis@htdevelopers.com>
2022-08-29 12:52:21 +0200

    Add space to model for readability

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,7 @@ class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
   belongs_to :user
   belongs_to :conversation, class_name: 'LiveChat::Conversation'
+
   validates :data, json: { schema: lambda {
     Rails.root.join("#{json_schema_path}/conversation/data.json").to_s
   }, message: lambda { |err|
