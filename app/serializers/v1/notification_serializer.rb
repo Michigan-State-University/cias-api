0505207c1fdf195a84ea8ef5ce6a2d0da58674da
Bartosz Kępka <86963976+bartosz-kepka-htd@users.noreply.github.com>
2022-09-13 14:45:47 +0200

    [CIAS30-2768] Add event to the notification serializer (#921)

--- app/serializers/v1/notification_serializer.rb
+++ app/serializers/v1/notification_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class V1::NotificationSerializer < V1Serializer
-  attributes :notifiable_type, :notifiable_id, :is_read, :created_at, :data
+  attributes :notifiable_type, :notifiable_id, :is_read, :created_at, :data, :event
 end
