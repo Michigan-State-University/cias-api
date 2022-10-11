c21aed4e3dc66bbb5ad2306b33b14acdf6ffb26d
Mateusz Miś <mateusz.mis@htdevelopers.com>
2022-10-11 11:33:29 +0200

    Rubocop

--- app/controllers/v1/user_sessions/questions_controller.rb
+++ app/controllers/v1/user_sessions/questions_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class V1::UserSessions::QuestionsController < V1Controller
-
   def index
     authorize! :read, user_session
 
     next_question = flow_service.user_session_question(preview_question_id)
     response = V1::Question::Response.call(next_question)
+
     render json: response
   end
 
