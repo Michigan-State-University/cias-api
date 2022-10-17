ceff1434f38fdf11fa1cd2042ba382027a2cbf17
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2022-10-17 13:42:59 +0200

    add tests

--- spec/jobs/multiple_characters/change_narrator_job_spec.rb
+++ spec/jobs/multiple_characters/change_narrator_job_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe MultipleCharacters::ChangeNarratorJob, type: :job do
   subject { described_class.perform_now(model, object_id, new_character, new_animations) }
 
+  let(:new_character) { 'emmi' }
   let(:new_animations) do
     {
       'eatCracker' => 'acknowledge',
@@ -10,15 +11,33 @@ RSpec.describe MultipleCharacters::ChangeNarratorJob, type: :job do
     }
   end
 
-  # before do
-  #   ActiveJob::Base.queue_adapter = :test
-  # end
   describe 'intervention' do
     let(:model) { Intervention.name }
     let(:object_id) { create(:intervention).id }
 
+    before do
+      ActiveJob::Base.queue_adapter = :test
+      allow(V1::MultipleCharacters::Interventions::ChangeService).to receive(:call).with(object_id, new_character, new_animations).and_return(true)
+    end
+
     it 'call correct service' do
-      change_service = class_double(V1::MultipleCharacters::Interventions::ChangeService)
+      subject
+      expect(V1::MultipleCharacters::Interventions::ChangeService).to have_received(:call)
+    end
+  end
+
+  describe 'session' do
+    let(:model) { Session.name }
+    let(:object_id) { create(:session).id }
+
+    before do
+      ActiveJob::Base.queue_adapter = :test
+      allow(V1::MultipleCharacters::Sessions::ChangeService).to receive(:call).with(object_id, new_character, new_animations).and_return(true)
+    end
+
+    it 'call correct service' do
+      subject
+      expect(V1::MultipleCharacters::Sessions::ChangeService).to have_received(:call)
     end
   end
 end
