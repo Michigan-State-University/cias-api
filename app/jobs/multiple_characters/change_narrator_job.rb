fa27aeafd7c20430de14e6a34aefc6872327fc22
Wiktoria Rozansla <wiktoria.rozanska@htdevelopers.com>
2022-10-21 12:37:25 +0200

    add default parameter

--- app/jobs/multiple_characters/change_narrator_job.rb
+++ app/jobs/multiple_characters/change_narrator_job.rb
@@ -3,7 +3,7 @@
 class MultipleCharacters::ChangeNarratorJob < ApplicationJob
   queue_as :default
 
-  def perform(user_email, model, object_id, new_character, new_animations)
+  def perform(user_email, model, object_id, new_character, new_animations = {})
     "V1::MultipleCharacters::#{model.pluralize}::ChangeService".safe_constantize.
       call(object_id, new_character, new_animations)
 
