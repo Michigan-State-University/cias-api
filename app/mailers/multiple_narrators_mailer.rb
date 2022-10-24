e5013d8f7a3e64e5841f134bbcba5224cd297a30
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2022-10-24 15:37:57 +0200

    change intervention.name to object.name

--- app/mailers/multiple_narrators_mailer.rb
+++ app/mailers/multiple_narrators_mailer.rb
@@ -4,6 +4,6 @@ class MultipleNarratorsMailer < ApplicationMailer
   def successfully_changed(user_email, object)
     @user_email = user_email
     @object = object
-    mail(to: user_email, subject: I18n.t('multiple_narrators_mailer.subject', intervention_name: @intervention.name))
+    mail(to: user_email, subject: I18n.t('multiple_narrators_mailer.subject', intervention_name: @object.name))
   end
 end
