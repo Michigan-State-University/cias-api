95cfffe3a8a7ad30a86102009bdb8644385c4333
Mateusz Mis <108629499+Misiatyy@users.noreply.github.com>
2022-10-28 13:46:26 +0200

    Remove notify_by column form navigator setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_03_082248) do
+ActiveRecord::Schema.define(version: 2022_10_28_112039) do
+
+>>>>>>> ffa7f1a5 (Remove notify_by column form navigator setup)
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pgcrypto"
