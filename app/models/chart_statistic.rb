dd65e091795c8a6c552d3aeb0a1fa9d60263126c
Maciej Górski <86961379+maciej-gorski@users.noreply.github.com>
2022-11-04 10:38:44 +0100

    added callback method to set filled_at date in chart statistic if its null, added specs (#1004)

--- app/models/chart_statistic.rb
+++ app/models/chart_statistic.rb
@@ -11,4 +11,12 @@ class ChartStatistic < ApplicationRecord
   scope :filled_between, ->(date_range) { where(filled_at: date_range) }
   scope :by_health_clinic_ids, ->(clinic_ids) { where(health_clinic_id: clinic_ids) }
   scope :ordered_data_for_chart, ->(chart_id) { where(chart_id: chart_id).order(filled_at: :asc) }
+
+  before_save :set_filled_at_date, if: -> { filled_at.nil? }
+
+  private
+
+  def set_filled_at_date
+    self.filled_at = created_at
+  end
 end
