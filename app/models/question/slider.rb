30348671eaa2b17617c605d29f46e8758885fdc6
Mateusz Mis <108629499+Misiatyy@users.noreply.github.com>
2022-11-15 11:16:14 +0100

    Allow to modify ranges in slider question

--- app/models/question/slider.rb
+++ app/models/question/slider.rb
@@ -3,6 +3,10 @@
 class Question::Slider < Question
   attribute :settings, :json, default: -> { assign_default_values('settings') }
 
+  validate :correct_range_format, if: :body_changed?
+
+  before_validation :change_range_to_integers, if: :body_changed?
+
   def self.assign_default_values(attr)
     super(attr).merge(
       { 'required' => true, 'show_number' => true }
@@ -34,8 +38,32 @@ class Question::Slider < Question
 
   def original_payload(row)
     row['original_text'] = {
+      'range_end' => row['range_end'],
+      'range_start' => row['range_start'],
       'end_value' => row['end_value'],
       'start_value' => row['start_value']
     }
+  end
+
+  def correct_range_format
+    body['data'][0]['payload'].tap do |question_payload|
+      return errors.add(:base, I18n.t('question.slider.invalid_range')) if question_payload['range_start'] >= question_payload['range_end']
+    end
+  end
+
+  def change_range_to_integers
+    new_body = body.deep_dup
+    question_payload = body['data'][0]['payload']
+    assign_default_range_values!(question_payload) if question_payload['range_start'].blank? || question_payload['range_end'].blank?
+    new_body['data'][0]['payload']['range_start'] = Integer(question_payload['range_start'])
+    new_body['data'][0]['payload']['range_end'] = Integer(question_payload['range_end'])
+    assign_attributes(body: new_body)
+  rescue ArgumentError
+    raise ActiveRecord::ActiveRecordError, I18n.t('question.slider.range_value_not_a_number')
+  end
+
+  def assign_default_range_values!(payload)
+    payload['range_start'] = 0
+    payload['range_end'] = 100
   end
 end
