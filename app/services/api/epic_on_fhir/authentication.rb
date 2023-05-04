b713ba4f26d3a862d766e3e1bd2760393915564a
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-05-04 09:40:26 +0200

    happy path: get access token

---
+++ app/services/api/epic_on_fhir/authentication.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class Api::EpicOnFhir::Authentication
+  ENDPOINT = ENV.fetch('EPIC_ON_FHIR_AUTHENTICATION_ENDPOINT')
+  AUTHENTICATION_ALGORITHM = ENV.fetch('EPIC_ON_FHIR_AUTHENTICATION_ALGORITHM')
+  CLIENT_ID = ENV.fetch('EPIC_ON_FHIR_CLIENT_ID')
+  GRANT_TYPE = 'client_credentials'
+  CLIENT_ASSERTION_TYPE = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+
+  def self.call
+    new.call
+  end
+
+  def call
+    response = Faraday.post(ENDPOINT) do |request|
+      request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+      request.body = URI.encode_www_form(params)
+    end
+
+    JSON.parse(response.body).symbolize_keys if response.status == 200
+  end
+
+  private
+
+  def params
+    {
+      grant_type: GRANT_TYPE,
+      client_assertion_type: CLIENT_ASSERTION_TYPE,
+      client_assertion: client_assertion
+
+    }
+  end
+
+  def client_assertion
+    JWT.encode jwt_payload, key, AUTHENTICATION_ALGORITHM, jwt_header
+  end
+
+  def jwt_header
+    {
+      alg: AUTHENTICATION_ALGORITHM,
+      typ: 'JWT'
+    }
+  end
+
+  def jwt_payload
+    {
+      iss: CLIENT_ID,
+      sub: CLIENT_ID,
+      aud: ENDPOINT,
+      jti: SecureRandom.uuid,
+      exp: (Time.zone.now + 5.minutes).to_i
+    }
+  end
+
+  def key
+    OpenSSL::PKey::RSA.new ENV.fetch('EPIC_ON_FHIR_PRIVATE_KEY'), ENV.fetch('EPIC_ON_FHIR_PUBLIC_KEY')
+  end
+end
