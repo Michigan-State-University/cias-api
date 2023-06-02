a7c2b9e70e08dae4cda26904653ee2ca1fe50e07
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-06-02 08:40:28 +0200

    fix for the bug -> only few models has method #ability_to_update_for? so we need to check if this one has before the check. This module is re-usable in many endpoints

--- app/controllers/concerns/resource/clone.rb
+++ app/controllers/concerns/resource/clone.rb
@@ -9,7 +9,8 @@ module Resource::Clone
       resource = model_constant.find(params[:id])
 
       return head :forbidden unless resource.ability_to_clone?
-      return head :forbidden unless resource.ability_to_update_for?(current_v1_user)
+
+      return head :forbidden if resource.respond_to?(:ability_to_update_for?) && !resource.ability_to_update_for?(current_v1_user)
 
       cloned_resource = resource.
         clone(params: clone_params)
