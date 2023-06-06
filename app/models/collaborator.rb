e0ab79314a9d97d8f5b18a6d09b7e0ea63e5af10
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2023-06-06 08:17:55 +0200

    [CIAS30-3410] add uniquess on relationship between intervention and user (collaborators) (#1207)

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -4,4 +4,6 @@ class Collaborator < ApplicationRecord
   has_paper_trail
   belongs_to :intervention, touch: true
   belongs_to :user
+
+  validates :user_id, uniqueness: { scope: :intervention_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_061453) do
+ActiveRecord::Schema.define(version: 2023_06_01_123537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -189,6 +189,7 @@ ActiveRecord::Schema.define(version: 2023_05_15_061453) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["intervention_id"], name: "index_collaborators_on_intervention_id"
+    t.index ["user_id", "intervention_id"], name: "index_collaborators_on_user_id_and_intervention_id", unique: true
     t.index ["user_id"], name: "index_collaborators_on_user_id"
   end
 
