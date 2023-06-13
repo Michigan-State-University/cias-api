b7e5f39ab42a3962bad961b542c8df4e655983a8
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2023-06-13 10:43:06 +0200

    add new values to settings in number questions - max and min length (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_063845) do
+ActiveRecord::Schema.define(version: 2023_06_05_101854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
