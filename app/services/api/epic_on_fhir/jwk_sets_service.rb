d4b4d3802651e3c7f40c859119ab5fcfee63d8f9
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-06-22 10:30:44 +0200

    add needed parameters to successfully decode provided client_assertion

--- app/services/api/epic_on_fhir/jwk_sets_service.rb
+++ app/services/api/epic_on_fhir/jwk_sets_service.rb
@@ -7,7 +7,13 @@ class Api::EpicOnFhir::JwkSetsService
 
   def call
     key = OpenSSL::PKey::RSA.new ENV.fetch('EPIC_ON_FHIR_PRIVATE_KEY'), ENV.fetch('EPIC_ON_FHIR_PUBLIC_KEY')
-    jwk = JWT::JWK.new(key)
+    jwk = JWT::JWK.new(key, optional_parameters)
     JWT::JWK::Set.new(jwk).export
+  end
+
+  private
+
+  def optional_parameters
+    { alg: ENV.fetch('EPIC_ON_FHIR_AUTHENTICATION_ALGORITHM'), kid: ENV.fetch('EPIC_ON_FHIR_KID') }
   end
 end
