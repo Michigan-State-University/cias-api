d2d22667eaa334ba6fdfb3a4a0424a0604c27406
msniecHTD <115976801+msniecHTD@users.noreply.github.com>
2023-06-27 12:21:10 +0200

    [CIAS-3419] - Fix cache key (#30)
    * [CIAS-3419] - Fix cache key
    
    * [CIAS-3419] - Make rubocop happy

--- app/serializers/v1/simple_intervention_serializer.rb
+++ app/serializers/v1/simple_intervention_serializer.rb
@@ -4,7 +4,11 @@ class V1::SimpleInterventionSerializer < V1Serializer
   include TeamCollaboratorsHelper
   attributes :id, :user_id, :name, :status, :created_at, :updated_at, :organization_id, :google_language_id
 
-  cache_options(store: Rails.cache, expires_in: 24.hours)  # temporary length, might be a subject to change
+  cache_options(store: Rails.cache, namespace: 'simple-intervention-serializer', expires_in: 24.hours)  # temporary length, might be a subject to change
+
+  attribute :user do |object|
+    object.user.as_json(only: %i[id email first_name last_name])
+  end
 
   attribute :sessions_size do |object|
     object.sessions.size
