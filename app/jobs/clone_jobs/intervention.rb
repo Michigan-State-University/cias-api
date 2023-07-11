ce3115c537177e7be183a5bef4aacd1fb0b76e71
oskardybowski <oskar.dybowski@htdhealth.com>
2023-07-11 13:53:40 +0200

    Refactor the clone job for interventions

--- app/jobs/clone_jobs/intervention.rb
+++ app/jobs/clone_jobs/intervention.rb
@@ -5,7 +5,7 @@ class CloneJobs::Intervention < CloneJob
     intervention = Intervention.find(intervention_id)
 
     cloned_interventions = intervention.clone(params: clone_params)
-    cloned_interventions = Array([:existing, cloned_interventions]) unless cloned_interventions.is_a?(Array)
+    cloned_interventions = Array(cloned_interventions) unless cloned_interventions.is_a?(Array)
 
     after_clone(intervention, cloned_interventions)
   rescue StandardError => e
@@ -15,18 +15,18 @@ class CloneJobs::Intervention < CloneJob
     logger.error 'ERROR-LOG-END'
     CloneMailer.error(user).deliver_now
 
-    cloned_interventions = Array([:existing, cloned_interventions]) unless cloned_interventions.is_a?(Array)
+    cloned_interventions = Array(cloned_interventions) unless cloned_interventions.is_a?(Array)
     clear_invalid_interventions(cloned_interventions)
   end
 
   private
 
   def after_clone(intervention, cloned_interventions)
-    cloned_interventions.each do |type, cloned_intervention|
+    cloned_interventions.each do |cloned_intervention|
       Intervention.reset_counters(cloned_intervention.id, :sessions)
       next unless cloned_intervention.user.email_notification
 
-      if type == :existing
+      if cloned_intervention.user.confirmed?
         CloneMailer.cloned_intervention(cloned_intervention.user, intervention.name, cloned_intervention.id).deliver_now
       else
         InterventionMailer.share_externally_and_registration(cloned_intervention, cloned_intervention.user.email).deliver_now
--- app/models/concerns/clone.rb
+++ app/models/concerns/clone.rb
@@ -5,27 +5,23 @@ module Clone
   include InvitationInterface
 
   def clone(params: {}, clean_formulas: true, position: nil, hidden: false)
-    if params[:emails].present?
+    emails = params[:emails]
+    if emails.present?
       interventions = []
 
-      existing_emails, non_existing_emails = split_emails_exist(params[:emails])
+      _existing_emails, non_existing_emails = split_emails_exist(emails)
       invite_non_existing_users(non_existing_emails, true, [:researcher])
 
-      user_ids = [existing_emails, non_existing_emails].map do |emails|
-        User.where(email: emails).limit_to_roles(%w[e_intervention_admin researcher]).pluck(:id)
-      end
+      user_ids = User.where(email: emails).limit_to_roles(%w[e_intervention_admin researcher]).pluck(:id)
 
-      user_ids.zip(%i[existing non_existing]).each do |ids, type|
-        ids.each do |id|
-          interventions
-            .push(
-              [type,
-               "Clone::#{de_constantize_modulize_name.classify}".
-                 safe_constantize.
-                 new(self, { user_id: id, clean_formulas: clean_formulas, position: position, hidden: hidden }).
-                 execute]
-            )
-        end
+      user_ids.each do |user_id|
+        interventions
+          .push(
+            "Clone::#{de_constantize_modulize_name.classify}".
+              safe_constantize.
+              new(self, { user_id: user_id, clean_formulas: clean_formulas, position: position, hidden: hidden }).
+              execute
+          )
       end
       interventions
     else
