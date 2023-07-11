8b5aac542581189c1808d69fda0e818d635b867b
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-07-11 12:12:49 +0200

    remove duplicates from some questions - related with cloning

--- app/models/question/single.rb
+++ app/models/question/single.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
 class Question::Single < Question
+  include ::Question::CloneableVariable
+
   attribute :settings, :json, default: -> { assign_default_values('settings') }
 
   def self.assign_default_values(attr)
     super(attr).merge(
       { 'proceed_button' => true, 'required' => true }
     )
-  end
-
-  def variable_clone_prefix(taken_variables)
-    return unless body['variable']['name'].presence
-
-    new_variable = "clone_#{body['variable']['name']}"
-    new_variable = variable_with_clone_index(taken_variables, body['variable']['name']) if taken_variables.include?(new_variable)
-    body['variable']['name'] = new_variable
   end
 
   def translate_body(translator, source_language_name_short, destination_language_name_short)
