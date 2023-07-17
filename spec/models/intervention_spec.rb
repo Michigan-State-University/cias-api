562cd07432d08db873beff13b020f13a1edb60c7
oskardybowski <oskar.dybowski@htdhealth.com>
2023-07-17 13:00:33 +0200

    remove the clone tests from the intervention model spec

--- spec/models/intervention_spec.rb
+++ spec/models/intervention_spec.rb
@@ -225,26 +225,6 @@ RSpec.describe Intervention, type: :model do
 
         expect(cloned_intervention.first.user_id).to eq(other_user.id)
       end
-
-      context 'when sharing an intervention to a researcher which has an activated account' do
-        let(:params) { { emails: [other_user.email] } }
-
-        it 'sends an email that invites only to check up the copy' do
-          allow(CloneMailer).to receive(:cloned_intervention).with(other_user, intervention.name, instance_of(String)).and_return(message_delivery)
-          CloneJobs::Intervention.perform_now(:user, intervention.id, params)
-        end
-      end
-
-      context 'when sharing an intervention to a researcher which hasn\'t yet activated an account' do
-        let(:unconfirmed_user) { create(:user, :researcher, :unconfirmed) }
-        let(:params) { { emails: [unconfirmed_user.email] } }
-
-        it 'sends an email that invites to make an account' do
-          allow(InterventionMailer).to receive(:share_externally_and_registration)
-                                         .with(instance_of(described_class), unconfirmed_user.email).and_return(message_delivery)
-          CloneJobs::Intervention.perform_now(:user, intervention.id, params)
-        end
-      end
     end
   end
 end
