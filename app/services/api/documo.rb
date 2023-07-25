a47db3432792d8e7563970a160fcbce4f1b00d95
oskardybowski <oskar.dybowski@htdhealth.com>
2023-07-25 15:13:11 +0200

    Revert the support for blobs and add support for attachments and logo in cover page

--- app/services/api/documo.rb
+++ app/services/api/documo.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::Documo
-  def send_fax(fax_number, logo_url, attachments = [])
-    Api::Documo::SendFax.call(fax_number, logo_url, attachments)
+  def send_fax(fax_number, attachments, include_cover_page = false, fields = {}, logo = nil)
+    Api::Documo::SendFax.call(fax_number, attachments, include_cover_page, fields, logo)
   end
 end
--- app/services/api/documo/send_fax.rb
+++ app/services/api/documo/send_fax.rb
@@ -2,17 +2,20 @@
 
 class Api::Documo::SendFax
   include Api::Request
+  include Rails.application.routes.url_helpers
 
   ENDPOINT = "#{ENV['BASE_DOCUMO_URL']}/v1/faxes"
 
-  def self.call(fax_number, logo_url, attachments)
-    new(fax_number, logo_url, attachments).call
+  def self.call(fax_number, attachments, include_cover_page, fields, logo)
+    new(fax_number, attachments, include_cover_page, fields, logo).call
   end
 
-  def initialize(fax_number, logo_url, attachments)
+  def initialize(fax_number, attachments, include_cover_page, fields, logo)
     @fax_number = fax_number
-    @logo_url = logo_url
     @attachments = attachments
+    @include_cover_page = include_cover_page
+    @fields = fields
+    @logo = logo
   end
 
   def call
@@ -37,18 +40,17 @@ class Api::Documo::SendFax
   def form_data
     data = [
       [:faxNumber, @fax_number],
-      [:coverPage, true],
-      [:coverPageId, '9998d00d-d8d6-4d60-90c5-a022051087e0'],
-      [:cf, {
-        logo: "<img src=\"#{@logo_url}\"></img>"
-      }.to_json]
+      [:coverPage, @include_cover_page],
+      [:coverPageId, '9998d00d-d8d6-4d60-90c5-a022051087e0']
     ]
 
+    unless @logo.nil?
+      cf = [:cf, { logo: "<img src=\"#{polymorphic_url(@logo)}\"></img>" }.to_json]
+      data.append(cf)
+    end
+
     @attachments.each do |attachment|
-      data.append([
-                    :attachments,
-                    Faraday::UploadIO.new(StringIO.new(attachment.download), attachment.content_type)
-                  ])
+      data.append([:attachmentUrls, polymorphic_url(attachment)])
     end
     data
   end
@@ -56,16 +58,9 @@ class Api::Documo::SendFax
   def build_multipart_data(boundary)
     body = form_data.each_with_object([]) do |(key, value), parts|
       parts << "--#{boundary}\r\n"
-      if value.is_a?(Faraday::UploadIO)
-        parts << "Content-Disposition: form-data; name=\"#{key}\"; filename=\"report.pdf\"\r\n"
-        parts << "Content-Type: #{value.content_type}\r\n"
-        parts << "\r\n"
-        parts << "#{value.read}\r\n"
-      else
-        parts << "Content-Disposition: form-data; name=\"#{key}\"\r\n"
-        parts << "\r\n"
-        parts << "#{value}\r\n"
-      end
+      parts << "Content-Disposition: form-data; name=\"#{key}\"\r\n"
+      parts << "\r\n"
+      parts << "#{value}\r\n"
     end
 
     body << "--#{boundary}--\r\n"
