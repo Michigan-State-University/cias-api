70530e9b9066b295ba174641945a43b9b36fb78e
oskardybowski <oskar.dybowski@htdhealth.com>
2023-07-31 13:42:18 +0200

    delete an overlooked debug statement

--- app/controllers/v1/sessions/report_templates_controller.rb
+++ app/controllers/v1/sessions/report_templates_controller.rb
@@ -37,8 +37,6 @@ class V1::Sessions::ReportTemplatesController < V1Controller
 
     return head :forbidden unless @session.ability_to_update_for?(current_v1_user)
 
-    Rails.logger.debug ">>> [#{self.class}>params] > #{params}"
-
     V1::ReportTemplates::Update.call(
       report_template,
       report_template_params
