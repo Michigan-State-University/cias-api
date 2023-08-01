5946be92b7e410b80351604ee2d9b5c2eda6c7db
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-01 16:03:35 +0200

    WIP

---
+++ app/services/v1/report_templates/duplicate_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class V1::ReportTemplates::DuplicateService
+  def self.call(report_template, target_session)
+    new(report_template, target_session).call
+  end
+
+  def initialize(report_template, target_session)
+    @report_template = report_template
+    @target_session = target_session
+  end
+
+  def call
+
+  end
+
+  attr_reader :report_template
+  attr_accessor :target_session
+
+  private
+end
