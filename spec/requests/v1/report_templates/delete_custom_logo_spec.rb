30360fab401ed0e4dcb50961496eab3c0edca734
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-01 16:03:47 +0200

    debug push

--- spec/requests/v1/report_templates/delete_custom_logo_spec.rb
+++ spec/requests/v1/report_templates/delete_custom_logo_spec.rb
@@ -8,15 +8,13 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
 
   let(:request) do
     delete v1_session_report_template_remove_cover_letter_custom_logo_path(
-      session_id: session.id, report_template_id: report_template.id
-    ), headers: headers
+             session_id: session.id, report_template_id: report_template.id
+           ), headers: headers
   end
 
   let!(:report_template) { create(:report_template, :with_logo, :with_custom_cover_letter_logo, session: session) }
 
-  context 'when deleting the custom cover letter logo' do
-    let!(:user) { create(:user, :confirmed, :admin) }
-
+  shared_examples 'can delete the custom cover page logo' do
     it 'removes the attachment' do
       expect { request }.to change(ActiveStorage::Attachment, :count).by(-1)
     end
@@ -27,9 +25,7 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
     end
   end
 
-  context 'when a user that\'s not the owner' do
-    let!(:user) { create(:user, :confirmed) }
-
+  shared_examples 'cannot delete the custom cover page logo' do
     it 'rejects the request' do
       request
       expect(response).to have_http_status(:forbidden)
@@ -38,5 +34,31 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
     it 'does not delete any attachments' do
       expect { request }.not_to change(ActiveStorage::Attachment, :count)
     end
+  end
+
+  context 'when deleting the custom cover letter logo' do
+    let!(:user) { create(:user, :confirmed, :admin) }
+
+    it_behaves_like 'can delete the custom cover page logo'
+  end
+
+  context 'when a user that\'s not the owner tries to delete the custom cover letter logo' do
+    let!(:user) { create(:user, :confirmed) }
+
+    it_behaves_like 'cannot delete the custom cover page logo'
+  end
+
+  context 'when the report is for a collaborative session' do
+    let(:user) { create(:user, :confirmed) }
+    let(:intervention) { create(:intervention, :with_collaborators, user: user, sessions: [session], current_editor: user) }
+
+    context 'when the user is the current editor' do
+
+      it_behaves_like 'can delete the custom cover page logo'
+    end
+
+    # context 'when the user is not the current editor' do
+    #   it_behaves_like 'cannot delete the custom cover page logo'
+    # end
   end
 end
