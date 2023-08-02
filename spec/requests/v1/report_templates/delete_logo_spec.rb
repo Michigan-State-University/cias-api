73c212d2414408ecbdcda553ffdf44b3d0bb1c2e
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-02 09:56:32 +0200

    add the corresponding tests for regular report template logo as well

--- spec/requests/v1/report_templates/delete_logo_spec.rb
+++ spec/requests/v1/report_templates/delete_logo_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover_letter_custom_logo', type: :request do
+RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_logo', type: :request do
   let(:intervention) { create(:intervention) }
   let(:session) { create(:session, intervention: intervention) }
   let(:user) { create(:user, :researcher, :confirmed) }
@@ -10,14 +10,14 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
   let(:headers) { user.create_new_auth_token }
 
   let(:request) do
-    delete v1_session_report_template_remove_cover_letter_custom_logo_path(
+    delete v1_session_report_template_remove_logo_path(
       session_id: session.id, report_template_id: report_template.id
     ), headers: headers
   end
 
-  let!(:report_template) { create(:report_template, :with_custom_cover_letter_logo, session: session) }
+  let!(:report_template) { create(:report_template, :with_logo, session: session) }
 
-  shared_examples 'can delete the custom cover page logo' do
+  shared_examples 'can delete the report logo' do
     it 'returns the response code for no content' do
       request
       expect(response).to have_http_status(:no_content)
@@ -28,7 +28,7 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
     end
   end
 
-  shared_examples 'cannot delete the custom cover page logo' do
+  shared_examples 'cannot delete the report logo' do
     it 'rejects the request' do
       request
       expect(response).to have_http_status(:forbidden)
@@ -39,20 +39,20 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
     end
   end
 
-  context 'when an admin deletes the custom cover letter logo' do
+  context 'when an admin deletes the report logo' do
     let!(:user) { create(:user, :admin, :confirmed) }
 
-    it_behaves_like 'can delete the custom cover page logo'
+    it_behaves_like 'can delete the report logo'
   end
 
   context 'when a researcher tries to delete the custom logo of a session which is not theirs' do
-    it_behaves_like 'cannot delete the custom cover page logo'
+    it_behaves_like 'cannot delete the report logo'
   end
 
-  context 'when a participant tries to delete the custom cover letter logo of their session' do
+  context 'when a participant tries to delete the report logo of their session' do
     let!(:intervention) { create(:intervention, user: user) }
 
-    it_behaves_like 'can delete the custom cover page logo'
+    it_behaves_like 'can delete the report logo'
   end
 
   context 'when the report is for a collaborative session' do
@@ -61,14 +61,14 @@ RSpec.describe 'DELETE v1/sessions/:session_id/report_templates/:id/remove_cover
     context 'when the user is the current editor' do
       let!(:intervention) { create(:intervention, :with_collaborators, user: user, current_editor: user) }
 
-      it_behaves_like 'can delete the custom cover page logo'
+      it_behaves_like 'can delete the report logo'
     end
 
     context 'when the user is not the current editor' do
       let(:other_user) { create(:user, :researcher, :confirmed) }
       let!(:intervention) { create(:intervention, :with_collaborators, user: user, current_editor: other_user) }
 
-      it_behaves_like 'cannot delete the custom cover page logo'
+      it_behaves_like 'cannot delete the report logo'
     end
   end
 end
