1fdb5ac2aa9191688c99ee22897c5c6aa1c1c2b4
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-08 10:35:59 +0200

    draft

---
+++ db/migrate/20230808053849_add_new_parameters_to_existing_third_party_questions.rb
@@ -0,0 +1,31 @@
+class AddNewParametersToExistingThirdPartyQuestions < ActiveRecord::Migration[6.1]
+  def up
+    p '-- TODO'
+    query = <<~SQL.squish
+      UPDATE questions
+      SET body = jsonb_set(body, '{variable}', '{"name": ""}', true )
+      WHERE type IN ('Question::ThirdParty')
+    SQL
+
+    query2 = <<~SQL.squish
+      UPDATE questions
+      SET
+        body = jsonb_set (
+          body,
+          '{data}',
+          (
+            SELECT
+              jsonb_agg (jsonb_set (element, '{numeric_value}', '""', true))
+            FROM
+              jsonb_array_elements (body -> 'data') AS element
+          )
+        )
+      WHERE type IN ('Question::ThirdParty')
+    SQL
+    ActiveRecord::Base.connection.exec_query(query2)
+  end
+
+  def down
+    # raise(ActiveRecord::IrreversibleMigration)
+  end
+end
