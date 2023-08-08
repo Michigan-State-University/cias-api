ea851b18bba9fec0eda393a07a0330cbc43da8c9
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-08 11:49:18 +0200

    combine two requests into one

--- db/migrate/20230808053849_add_new_parameters_to_existing_third_party_questions.rb
+++ db/migrate/20230808053849_add_new_parameters_to_existing_third_party_questions.rb
@@ -1,28 +1,22 @@
 class AddNewParametersToExistingThirdPartyQuestions < ActiveRecord::Migration[6.1]
   def up
-    p '-- TODO'
+    p '-- add new properties to body in third party question'
     query = <<~SQL.squish
-      UPDATE questions
-      SET body = jsonb_set(body, '{variable}', '{"name": ""}', true )
-      WHERE type IN ('Question::ThirdParty')
-    SQL
-
-    query2 = <<~SQL.squish
       UPDATE questions
       SET
         body = jsonb_set (
-          body,
+          jsonb_set(body, '{variable}', '{"name": ""}', true ),
           '{data}',
           (
             SELECT
-              jsonb_agg (jsonb_set (element, '{numeric_value}', '""', true))
+              jsonb_agg(jsonb_set (element, '{numeric_value}', '""', true))
             FROM
-              jsonb_array_elements (body -> 'data') AS element
+              jsonb_array_elements(body -> 'data') AS element
           )
         )
       WHERE type IN ('Question::ThirdParty')
     SQL
-    ActiveRecord::Base.connection.exec_query(query2)
+    ActiveRecord::Base.connection.exec_query(query)
   end
 
   def down
