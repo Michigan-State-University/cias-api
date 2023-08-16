c83d9f51ae08a3aa6eebb071e8dfdbe3dcec17f3
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-16 13:13:50 +0200

    fix CSV generation after merging

--- app/models/intervention/csv/harvester.rb
+++ app/models/intervention/csv/harvester.rb
@@ -86,6 +86,8 @@ class Intervention::Csv::Harvester
         metadata(user_session.session, user_session, row_index, user_session.number_of_attempts, user_session.session.multiple_fill)
         quick_exit(user_session.session, row_index, user_session, user_session.number_of_attempts, user_session.session.multiple_fill)
       end
+
+      fill_hf_initial_screen(row_index, grouped_user_sessions.second.first)
     end
   end
 
@@ -147,6 +149,28 @@ class Intervention::Csv::Harvester
     return [] if hf_initial_question.nil?
 
     hf_initial_question.csv_header_names
+  end
+
+  def fill_hf_initial_screen(row_index, user_session)
+    return unless user_session.session.intervention.hfhs_access
+
+    question = ::Question::HenryFordInitial.joins(:answers).find_by(answers: { user_session: user_session })
+    attrs = question&.csv_decoded_attrs
+    patient_details = patient_details(user_session, attrs)
+    return if patient_details.blank?
+
+    attrs = question.rename_attrs(attrs)
+    attrs.each_with_index do |column, index|
+      var_index = header.index("henry_ford_health.#{column}")
+      next if var_index.nil?
+
+      rows[row_index][var_index] = patient_details[index]
+    end
+  end
+
+  def patient_details(user_session, attrs)
+    details = user_session.user.hfhs_patient_detail&.attributes
+    details&.fetch_values(*attrs)
   end
 
   def number_of_attempts(session)
