7ccf92e0f3ff6f8ba60654006eeaba476c500212
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-17 11:27:18 +0200

    add tests and small modification in DataClearJobs::InformAndSchedule job

--- app/jobs/data_clear_jobs/inform_and_schedule.rb
+++ app/jobs/data_clear_jobs/inform_and_schedule.rb
@@ -14,8 +14,8 @@ class DataClearJobs::InformAndSchedule < ApplicationJob
   def send_emails_to_third_party_users!(intervention)
     User.joins(generated_reports_third_party_users: :generated_report)
         .where(generated_report: { report_template_id: intervention.sessions.joins(:report_templates)
-                                                                   .select('report_templates.id') }).uniq.find_each do |third_party|
-      InterventionMailer::ClearDataMailer.inform(third_party, intervention).deliver_now
+                                                                   .select('report_templates.id') }).distinct.find_each do |third_party|
+      InterventionMailer::ClearDataMailer.inform(third_party, intervention).deliver_now if third_party.confirmed?
     end
   end
 
@@ -23,10 +23,10 @@ class DataClearJobs::InformAndSchedule < ApplicationJob
     intervention.user_interventions.find_each do |user_intervention|
       user = user_intervention.user
       if user.role?('guest')
-        User.where(user_intervention.user_sessions.joins(:generated_reports).select('generated_reports.participant_id')).find_each do |user|
-          next unless user.confirmed?
+        User.where(id: user_intervention.user_sessions.joins(:generated_reports).select('generated_reports.participant_id')).find_each do |created_user|
+          next unless created_user.confirmed?
 
-          InterventionMailer::ClearDataMailer.inform(user, intervention).deliver_now
+          InterventionMailer::ClearDataMailer.inform(created_user, intervention).deliver_now
         end
       else
         InterventionMailer::ClearDataMailer.inform(user, intervention).deliver_now
