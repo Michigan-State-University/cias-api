f6876fac75c4246e6972ad8c2be6c7979038056f
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2023-08-18 12:07:17 +0200

    [CIAS30-3580] create notification after clear the data (#95)
    * job to send emails and schedule next job
    
    * add tests and small modification in DataClearJobs::InformAndSchedule job
    
    * add header to email template
    
    * create notification after clear the data

--- app/jobs/data_clear_jobs/clear_user_data.rb
+++ app/jobs/data_clear_jobs/clear_user_data.rb
@@ -11,6 +11,7 @@ class DataClearJobs::ClearUserData < ApplicationJob
       delete_quest_users_without_any_intervention!
 
       intervention.sensitive_data_removed!
+      create_notification!(intervention)
     end
   end
 
@@ -18,5 +19,10 @@ class DataClearJobs::ClearUserData < ApplicationJob
 
   def delete_quest_users_without_any_intervention!
     User.left_joins(:user_interventions).where(roles: ['guest'], user_interventions: { id: nil }).destroy_all
+  end
+
+  def create_notification!(intervention)
+    Notification.create!(user: intervention.user, notifiable: intervention, event: :sensitive_data_removed,
+                         data: { intervention_name: intervention.name, intervention_id: intervention.id })
   end
 end
