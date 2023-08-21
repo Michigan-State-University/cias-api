e9e28bf3dc3940e7ca7167c8939bb65b4f24ab72
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-21 15:51:26 +0200

    remove the `starred` field from the serializers

--- app/serializers/v1/simple_intervention_serializer.rb
+++ app/serializers/v1/simple_intervention_serializer.rb
@@ -2,7 +2,7 @@
 
 class V1::SimpleInterventionSerializer < V1Serializer
   include TeamCollaboratorsHelper
-  attributes :id, :user_id, :name, :status, :created_at, :updated_at, :organization_id, :google_language_id, :starred
+  attributes :id, :user_id, :name, :status, :created_at, :updated_at, :organization_id, :google_language_id
 
   cache_options(store: Rails.cache, namespace: 'simple-intervention-serializer', expires_in: 24.hours)  # temporary length, might be a subject to change
 
