4a8312dd2a366c63d1ae8ddb0638926b42d65942
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-21 15:52:00 +0200

    update the interventions controller to support starring

--- app/controllers/v1/interventions_controller.rb
+++ app/controllers/v1/interventions_controller.rb
@@ -10,14 +10,15 @@ class V1::InterventionsController < V1Controller
   end
 
   def show
-    render json: serialized_response(intervention_load, controller_name.classify, params: { current_user_id: current_v1_user.id })
+    render json: serialized_hash(intervention_load, controller_name.classify, params: { current_user_id: current_v1_user.id })
+                   .deep_merge(starred_hash(intervention_load)).to_json
   end
 
   def create
     authorize! :create, Intervention
 
     intervention = current_v1_user.interventions.create!(intervention_params)
-    render json: serialized_response(intervention), status: :created
+    render json: serialized_hash(intervention).deep_merge(starred_hash(intervention)).to_json, status: :created
   end
 
   def update
@@ -25,8 +26,9 @@ class V1::InterventionsController < V1Controller
     authorize! :update, intervention_load
     return head :forbidden unless intervention_load.ability_to_update_for?(current_v1_user)
 
-    intervention = V1::Intervention::Update.new(intervention_load, intervention_params).execute
-    render json: serialized_response(intervention)
+    intervention = V1::Intervention::Update.new(intervention_load, intervention_params, current_v1_user).execute
+
+    render json: serialized_hash(intervention).deep_merge(starred_hash(intervention)).to_json
   end
 
   def clone
@@ -65,14 +67,30 @@ class V1::InterventionsController < V1Controller
     redirect_to(ENV['APP_HOSTNAME'] + Rails.application.routes.url_helpers.rails_blob_path(intervention_load.conversations_transcript, only_path: true))
   end
 
+  def make_starred
+    authorize! :read, Intervention
+
+    current_v1_user.stars.find_or_create_by(intervention_id: intervention_load.id)
+
+    render json: serialized_hash(intervention_load).deep_merge(starred_hash(intervention_load)).to_json
+  end
+
+  def make_unstarred
+    authorize! :read, Intervention
+
+    current_v1_user.stars.delete_by(intervention_id: intervention_load.id)
+
+    render json: serialized_hash(intervention_load).deep_merge(starred_hash(intervention_load)).to_json
+  end
+
   private
 
   def interventions_scope
     @interventions_scope ||= Intervention.accessible_by(current_ability)
-                .order(created_at: :desc)
-                .includes(%i[user reports_attachments files_attachments google_language logo_attachment logo_blob collaborators
+                                         .order(created_at: :desc)
+                                         .includes(%i[user reports_attachments files_attachments google_language logo_attachment logo_blob collaborators
                              conversations_transcript_attachment])
-                .only_visible
+                                         .only_visible
   end
 
   def intervention_load
@@ -81,15 +99,15 @@ class V1::InterventionsController < V1Controller
 
   def intervention_params
     if params[:id].present? && intervention_load.published?
-      params.require(:intervention).permit(:status, :starred, :cat_mh_pool, :is_access_revoked, :live_chat_enabled, location_ids: [])
+      params.require(:intervention).permit(:status, :cat_mh_pool, :is_access_revoked, :live_chat_enabled, location_ids: [])
     elsif current_v1_user.admin?
-      params.require(:intervention).permit(:name, :status, :type, :shared_to, :additional_text, :organization_id, :starred, :google_language_id, :cat_mh_application_id,
-                                           :cat_mh_organization_id, :cat_mh_pool, :is_access_revoked, :license_type, :quick_exit, :hfhs_access,
-                                           :live_chat_enabled, location_ids: [])
+      params.require(:intervention).permit(:name, :status, :type, :shared_to, :additional_text, :organization_id, :google_language_id,
+                                           :cat_mh_application_id, :cat_mh_organization_id, :cat_mh_pool, :is_access_revoked, :license_type, :quick_exit,
+                                           :hfhs_access, :live_chat_enabled, location_ids: [])
     else
-      params.require(:intervention).permit(:name, :status, :type, :shared_to, :additional_text, :organization_id, :starred, :google_language_id, :cat_mh_application_id,
-                                           :cat_mh_organization_id, :cat_mh_pool, :is_access_revoked, :license_type, :live_chat_enabled, :quick_exit,
-                                           location_ids: [])
+      params.require(:intervention).permit(:name, :status, :type, :shared_to, :additional_text, :organization_id, :google_language_id,
+                                           :cat_mh_application_id, :cat_mh_organization_id, :cat_mh_pool, :is_access_revoked, :license_type, :live_chat_enabled,
+                                           :quick_exit, location_ids: [])
     end
   end
 
@@ -113,5 +131,13 @@ class V1::InterventionsController < V1Controller
       question: [],
       sms_plan: []
     }
+  end
+
+  def starred?(intervention)
+    Star.find_by(user_id: current_v1_user.id, intervention_id: intervention.id).present?
+  end
+
+  def starred_hash(intervention)
+    { data: { attributes: { starred: starred?(intervention) } } }
   end
 end
