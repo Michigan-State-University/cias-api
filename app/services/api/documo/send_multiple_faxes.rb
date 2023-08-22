c42a876e1632ef7bcdeeaf14094596f916899364
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-22 07:10:26 +0200

    add dynamic link instead of static one

--- app/services/api/documo/send_multiple_faxes.rb
+++ app/services/api/documo/send_multiple_faxes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::Documo::SendMultipleFaxes
+  include Rails.application.routes.url_helpers
+
   ENDPOINT = "#{ENV.fetch('BASE_DOCUMO_URL')}/v1/faxes/multiple"
 
   def self.call(fax_numbers, attachments, include_cover_page, fields, logo)
@@ -20,10 +22,13 @@ class Api::Documo::SendMultipleFaxes
   def call
     response = connection.post do |req|
       req.headers['Authorization'] = "Basic #{ENV.fetch('DOCUMO_API_KEY')}"
-      req.headers['Content-Type'] = "multipart/form-data"
+      req.headers['Content-Type'] = 'multipart/form-data'
       req.body = form_data
     end
-    JSON.parse(response.body)
+    {
+      status: response.status,
+      body: JSON.parse(response.body)
+    }
   end
 
   def connection
@@ -40,8 +45,8 @@ class Api::Documo::SendMultipleFaxes
       coverPage: @include_cover_page,
       coverPageId: ENV.fetch('DOCUMO_COVER_PAGE_ID'),
       recipientFax: fax_numbers.join(', '),
-      cf: { logo: '<img src="https://picsum.photos/200"></img>' },
-      attachments: attachments.map{ |file| Faraday::UploadIO.new(StringIO.new(file.download), file.content_type, file.filename.to_s) },
+      cf: { logo: "<img src=\"#{polymorphic_url(logo)}\"></img>" },
+      attachments: attachments.map { |file| Faraday::UploadIO.new(StringIO.new(file.download), file.content_type, file.filename.to_s) }
     }
   end
 end
