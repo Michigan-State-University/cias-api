6acd0ab2703e10fe60e8ec56f74077b88a8b32fa
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-22 07:46:42 +0200

    rubocop -A

--- app/serializers/v1/report_template_serializer.rb
+++ app/serializers/v1/report_template_serializer.rb
@@ -2,7 +2,7 @@
 
 class V1::ReportTemplateSerializer < V1Serializer
   attributes :name, :report_for, :logo_url, :cover_letter_custom_logo_url, :summary, :session_id, :original_text,
-             :has_cover_letter, :cover_letter_logo_type, :cover_letter_description, :cover_letter_sender, 
+             :has_cover_letter, :cover_letter_logo_type, :cover_letter_description, :cover_letter_sender,
              :original_text, :is_duplicated_from_other_session, :duplicated_from_other_session_warning_dismissed
   has_many :sections, serializer: V1::ReportTemplate::SectionSerializer
   has_many :variants, serializer: V1::ReportTemplate::Section::VariantSerializer
