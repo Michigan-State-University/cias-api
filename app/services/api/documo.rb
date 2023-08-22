45770f79886c75b1c1718d3e5945ff30acfd2a54
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-22 07:12:37 +0200

    rename the method in API::DOCUMO service

--- app/services/api/documo.rb
+++ app/services/api/documo.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::Documo
-  def send_fax(fax_numbers, attachments, include_cover_page = false, fields = {}, logo = nil)
-    Api::Documo::SendFax.call(fax_numbers, attachments, include_cover_page, fields, logo)
+  def send_faxes(fax_numbers, attachments, include_cover_page = false, fields = {}, logo = nil)
+    Api::Documo::SendMultipleFaxes.call(fax_numbers, attachments, include_cover_page, fields, logo)
   end
 end
