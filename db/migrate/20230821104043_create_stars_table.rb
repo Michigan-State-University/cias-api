19e338ce8441c5b2eec8b00d109ce070ec3dc9c9
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-23 11:35:29 +0200

    change the migration

--- db/migrate/20230821104043_create_stars_table.rb
+++ db/migrate/20230821104043_create_stars_table.rb
@@ -1,15 +1,9 @@
 class CreateStarsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :stars, id: :uuid, default: 'uuid_generate_v4()', null: false do |t|
-      t.uuid :user_id, null: false
-      t.uuid :intervention_id, null: false
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.references :intervention, null: false, foreign_key: true, type: :uuid
+      t.index %w[user_id intervention_id], unique: true
     end
-
-    add_index :stars, :user_id
-    add_index :stars, :intervention_id
-    add_index :stars, %i[user_id intervention_id], unique: true
-
-    add_foreign_key :stars, :users
-    add_foreign_key :stars, :interventions
   end
 end
