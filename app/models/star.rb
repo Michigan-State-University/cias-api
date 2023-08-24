59734f558c85f18aef56d7ba6e75213b55b880dc
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-24 08:37:27 +0200

    added validation in the star model

--- app/models/star.rb
+++ app/models/star.rb
@@ -4,4 +4,6 @@ class Star < ApplicationRecord
   has_paper_trail
   belongs_to :user, inverse_of: :stars
   belongs_to :intervention, inverse_of: :stars
+
+  validates :user_id, uniqueness: { scope: :intervention_id }
 end
