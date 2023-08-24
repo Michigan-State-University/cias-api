2ed99de41574bb2a9c7a71f86ee0d57236781df0
oskardybowski <oskar.dybowski@htdhealth.com>
2023-08-24 16:52:45 +0200

    add tests for filtering by stars

--- spec/requests/v1/interventions/index_spec.rb
+++ spec/requests/v1/interventions/index_spec.rb
@@ -201,6 +201,40 @@ RSpec.describe 'GET /v1/interventions', type: :request do
     end
   end
 
+  context 'when filtering by whether an intervention is starred' do
+    let!(:starred_intervention) { create(:intervention, user: user) }
+    let!(:not_starred_intervention) { create(:intervention, user: user) }
+
+    before do
+      Star.create(user_id: user.id, intervention_id: starred_intervention.id)
+      get v1_interventions_path, params: params, headers: user.create_new_auth_token
+    end
+
+    context 'return all interventions when starred not specified' do
+      let(:params) { {} }
+
+      it 'returns correct interventions' do
+        expect(json_response['data'].pluck('id')).to include(starred_intervention.id).and include(not_starred_intervention.id)
+      end
+    end
+
+    context 'return only starred interventions' do
+      let(:params) { { starred: true } }
+
+      it 'returns correct interventions' do
+        expect(json_response['data'].pluck('id')).to include(starred_intervention.id).and not_include(not_starred_intervention.id)
+      end
+    end
+
+    context 'return only not starred interventions' do
+      let(:params) { { starred: false } }
+
+      it 'returns correct interventions' do
+        expect(json_response['data'].pluck('id')).to not_include(starred_intervention.id).and include(not_starred_intervention.id)
+      end
+    end
+  end
+
   context 'when some interventions are starred' do
     let(:other_researcher) { create(:user, :confirmed, :researcher) }
 
