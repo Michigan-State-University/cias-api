b95ad64011c544210a3195b2b7895bd759ebfeb1
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-08-31 09:58:58 +0200

    WIP

--- app/jobs/sms_plans/send_sms_job.rb
+++ app/jobs/sms_plans/send_sms_job.rb
@@ -3,10 +3,10 @@
 class SmsPlans::SendSmsJob < ApplicationJob
   queue_as :sms_plans
 
-  def perform(number, content, attachment_url, user_id, is_alert = false)
+  def perform(number, content, attachment_url, user_id, is_alert = false, user_session_id = nil)
     unless is_alert
       user = User.find(user_id)
-      return unless user.sms_notification
+      return unless user&.sms_notification
     end
 
     sms = Message.create(phone: number, body: content, attachment_url: attachment_url)
