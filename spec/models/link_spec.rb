2805ede2fe0c6db25d5e69852b6c3214a46a962f
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2023-08-31 13:26:28 +0200

    [CIAS30-3615] generate a short link for attachments  (#115)

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.describe Link, type: :model do
+  it { validate_uniqueness_of(:slug) }
+  it { validate_presence_of(:url) }
+  it { validate_length_of(:length).is_at_least(3) }
+  it { validate_length_of(:length).is_at_most(255) }
+  it { should allow_value('https://picsum.photos/200').for(:url) }
+
+  describe 'callbacks' do
+    it 'generate a slug before validation if blank' do
+      link = build(:link, slug: nil)
+      link.valid?
+      expect(link.slug).not_to be_nil
+    end
+  end
+
+  describe 'methods' do
+    let!(:link) { create(:link) }
+    let(:url) { link.url }
+    let(:slug) { 'slug' }
+
+    it '#short' do
+      expect(link.short).to eq(Rails.application.routes.url_helpers.v1_short_url(slug: link.slug))
+    end
+
+    it '#shorten' do
+      short_url = described_class.shorten(url)
+      expect(short_url).not_to be_nil
+      expect(described_class.find_by(url: url)).to eq(link)
+    end
+
+    it '#short with custom slug' do
+      short_url = described_class.shorten(url, slug)
+      expect(short_url).to eq(Rails.application.routes.url_helpers.v1_short_url(slug: slug))
+    end
+
+    context 'when slug is too long' do
+      let(:slug) { SecureRandom.base58(256) }
+
+      it '#short with custom slug' do
+        expect { described_class.shorten(url, slug) }.to raise_error(ActiveRecord::ActiveRecordError)
+      end
+    end
+  end
+end
