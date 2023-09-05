abf3ba6cf1943c9bd956315a332589da2cedc3bf
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-05 09:14:24 +0200

    refactor the rake task

--- db/seeds/interventions/chart_data_seed.rb
+++ db/seeds/interventions/chart_data_seed.rb
@@ -3,33 +3,35 @@
 def main
   clear_table_cache
 
-  organization
-  session_fruits
-  session_months
+  organization = create_organization!
+  researcher = create_researcher!(organization)
+  create_e_intervention_admin_organization!(researcher, organization)
+
+  participants = create_participants!
+
+  intervention = create_intervention!(researcher, organization)
+  create_fruits_session!(intervention, participants, organization)
+  create_months_session!(intervention, participants, organization)
 end
 
-def researcher
-  @researcher ||= User.new(
+def create_researcher!(organization)
+  researcher = User.new(
     first_name: 'Chart',
     last_name: 'Test',
     email: 'cias-team+charts_test_researcher@htdevelopers.com',
     terms: true, terms_confirmed_at: DateTime.now, confirmed_at: DateTime.now,
     password: "Aa1#{SecureRandom.base58(16)}!",
     roles: %i[researcher e_intervention_admin],
-    organizable_id: organization.id, organizable_type: 'Organization'
-  ).tap do |researcher|
-    researcher.confirm
-    researcher.save!
+    organizable: organization
+  )
 
-    EInterventionAdminOrganization.create!(
-      user_id: researcher.id,
-      organization_id: organization.id
-    )
-  end
+  researcher.confirm
+  researcher.save!
+  researcher
 end
 
-def participants
-  @participants ||= 200.times.each_with_object([]) do |index, array|
+def create_participants!
+  200.times.each_with_object([]) do |index, array|
     participant = User.new(
       first_name: 'Participant',
       last_name: "Number #{index}",
@@ -45,19 +47,17 @@ def participants
   end
 end
 
-def intervention
-  @intervention ||= Intervention.create!(
+def create_intervention!(owner, organization)
+  Intervention.create!(
     name: 'Months and fruit - TEST INTERVENTION',
-    user_id: researcher.id,
+    user_id: owner.id,
     status: :published,
     organization: organization
   )
 end
 
-def session_fruits
-  return @session_fruits if @session_fruits.present?
-
-  @session_fruits = Session.create!(
+def create_fruits_session!(intervention, participants, organization)
+  fruits_session = Session.create!(
     intervention_id: intervention.id,
     name: 'Random fruit over a 1 year period'
   )
@@ -65,7 +65,7 @@ def session_fruits
   question = Question::Single.create!(
     title: '<h2>Fruit Question</h2>',
     subtitle: '<p>What\'s your favorite fruit?</p>',
-    question_group_id: QuestionGroup.create!(title: 'Test Group', session_id: @session_fruits.id).id,
+    question_group_id: QuestionGroup.create!(title: 'Test Group', session_id: fruits_session.id).id,
     body: { 'data' => %w[Apple Banana Grape Pear].each.with_index(1).map do |fruit, index|
       { 'value' => index.to_s, 'payload' => "<p>#{fruit}</p>" }
     end, 'variable' => { 'name' => 'fruit' } }
@@ -78,9 +78,10 @@ def session_fruits
 
     user_session = UserSession.create!(
       user_id: participant.id,
-      session_id: @session_fruits.id,
+      session_id: fruits_session.id,
       user_intervention_id: user_intervention.id,
-      health_clinic_id: Random.rand(1..3) == 1 ? organization.health_clinics.first.id : organization.health_clinics.second.id
+      health_clinic_id: Random.rand(1..3) == 1 ? organization.health_clinics.first.id : organization.health_clinics.second.id,
+      created_at: finished_at - 10.minutes
     )
     Answer::Single.create!(
       question_id: question.id,
@@ -91,10 +92,8 @@ def session_fruits
   end
 end
 
-def session_months
-  return @session_months if @session_months.present?
-
-  @session_months = Session.create(
+def create_months_session!(intervention, participants, organization)
+  months_session = Session.create(
     intervention_id: intervention.id,
     name: 'Current over a 2 year period'
   )
@@ -102,22 +101,23 @@ def session_months
   question = Question::Single.create!(
     title: '<h2>Fruit Question</h2>',
     subtitle: '<p>What\'s the current month?</p>',
-    question_group_id: QuestionGroup.find_or_create_by!(title: 'Test Group', session_id: @session_months.id).id,
+    question_group_id: QuestionGroup.find_or_create_by!(title: 'Test Group', session_id: months_session.id).id,
     body: { 'data' => %w[January February March April May June July August September October November December].each.with_index(1).map do |month, index|
       { 'value' => index.to_s, 'payload' => "<p>#{month}</p>" }
     end, 'variable' => { 'name' => 'month' } }
   )
 
+  finished_at = Random.rand(1..730).days.ago
+
   participants.each do |participant|
     user_intervention = UserIntervention.create!(user_id: participant.id, intervention_id: intervention.id)
     user_session = UserSession.create!(
       user_id: participant.id,
-      session_id: @session_months.id,
+      session_id: months_session.id,
       user_intervention_id: user_intervention.id,
-      health_clinic_id: organization.health_clinics.first.id
+      health_clinic_id: organization.health_clinics.first.id,
+      created_at: finished_at - 10.minutes
     )
-
-    finished_at = Random.rand(1..730).days.ago
 
     Answer::Single.create!(
       question_id: question.id,
@@ -128,13 +128,19 @@ def session_months
   end
 end
 
-def organization
-  return @organization if @organization.present?
-
-  @organization = Organization.create!(name: 'Chart Test Organization')
-  health_system = HealthSystem.create!(name: 'Chart Test Health System', organization_id: @organization.id)
+def create_organization!
+  organization = Organization.create!(name: 'Chart Test Organization')
+  health_system = HealthSystem.create!(name: 'Chart Test Health System', organization_id: organization.id)
   HealthClinic.create!(name: 'Chart Test Clinic 1', health_system_id: health_system.id)
   HealthClinic.create!(name: 'Chart Test Clinic 2', health_system_id: health_system.id)
+  organization
+end
+
+def create_e_intervention_admin_organization!(user, organization)
+  EInterventionAdminOrganization.create!(
+    user_id: user.id,
+    organization_id: organization.id
+  )
 end
 
 private
