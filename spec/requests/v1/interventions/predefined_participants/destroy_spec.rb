2c8ca7b14b50334fba52eaf4172fb627b5e71e47
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-07 13:14:34 +0200

    Add tests

--- spec/requests/v1/interventions/predefined_participants/destroy_spec.rb
+++ spec/requests/v1/interventions/predefined_participants/destroy_spec.rb
@@ -2,23 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe 'PATCH /v1/interventions/:intervention_id/predefined_participants/:id', type: :request do
+RSpec.describe 'DELETE /v1/interventions/:intervention_id/predefined_participants/:id', type: :request do
   let(:request) do
-    patch v1_intervention_predefined_participant_path(intervention_id: intervention.id, id: user.id), params: params,
-                                                                                                      headers: current_user.create_new_auth_token
+    delete v1_intervention_predefined_participant_path(intervention_id: intervention.id, id: user.id), headers: current_user.create_new_auth_token
   end
   let!(:intervention) { create(:intervention, user: researcher) }
   let(:researcher) { create(:user, :researcher, :confirmed) }
   let(:current_user) { researcher }
   let(:user) { create(:user, :predefined_participant, :with_phone) }
-  let(:params) do
-    {
-      predefined_user: {
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name
-      }
-    }
-  end
 
   before do
     PredefinedUserParameter.create(user: user, intervention: intervention)
@@ -26,28 +17,20 @@ RSpec.describe 'PATCH /v1/interventions/:intervention_id/predefined_participants
 
   it 'return correct status' do
     request
-    expect(response).to have_http_status(:ok)
+    expect(response).to have_http_status(:no_content)
   end
 
-  it 'return correct body' do
-    request
-    expect(json_response['data']['attributes'].keys).to match_array(%w[full_name first_name last_name phone slug health_clinic_id])
+  it 'does not delete the user' do
+    expect { request }.not_to change(User, :count)
   end
 
   it_behaves_like 'users without access'
 
-  context 'researcher activates the account' do
-    let(:user) { create(:user, :predefined_participant, active: false) }
-    let(:params) do
-      {
-        predefined_user: {
-          active: true
-        }
-      }
-    end
+  context 'researcher deactivates the account' do
+    let(:user) { create(:user, :predefined_participant, active: true) }
 
     it 'activates the user' do
-      expect { request }.to change { user.reload.active }.to(true)
+      expect { request }.to change { user.reload.active }.to(false)
     end
   end
 
@@ -68,7 +51,7 @@ RSpec.describe 'PATCH /v1/interventions/:intervention_id/predefined_participants
 
       it 'current editor has access to the action' do
         request
-        expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:no_content)
       end
     end
   end
