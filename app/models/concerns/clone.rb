e058f8e92dfa9dfe5bb8d43c46d3f7c855ff370b
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-11 14:42:39 +0200

    fix variable name

--- app/models/concerns/clone.rb
+++ app/models/concerns/clone.rb
@@ -9,7 +9,7 @@ module Clone
     current_user_id = params[:user_id]
 
     if emails.blank?
-      if user_id.present?
+      if current_user_id.present?
         return clone_module.new(self, user_id: current_user_id, clean_formulas: clean_formulas, position: position, params: params, hidden: hidden).execute
       end
 
