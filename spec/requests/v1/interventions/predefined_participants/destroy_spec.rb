872aea08c5287bc94eb35551d7e860d459e9d11c
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-11 11:11:09 +0200

    fix tests

--- spec/requests/v1/interventions/predefined_participants/destroy_spec.rb
+++ spec/requests/v1/interventions/predefined_participants/destroy_spec.rb
@@ -6,14 +6,10 @@ RSpec.describe 'DELETE /v1/interventions/:intervention_id/predefined_participant
   let(:request) do
     delete v1_intervention_predefined_participant_path(intervention_id: intervention.id, id: user.id), headers: current_user.create_new_auth_token
   end
-  let!(:intervention) { create(:intervention, user: researcher) }
+  let!(:intervention) { create(:intervention, :with_predefined_participants, user: researcher) }
   let(:researcher) { create(:user, :researcher, :confirmed) }
   let(:current_user) { researcher }
-  let(:user) { create(:user, :predefined_participant, :with_phone) }
-
-  before do
-    PredefinedUserParameter.create(user: user, intervention: intervention)
-  end
+  let!(:user) { intervention.predefined_users.first }
 
   it 'return correct status' do
     request
@@ -27,15 +23,13 @@ RSpec.describe 'DELETE /v1/interventions/:intervention_id/predefined_participant
   it_behaves_like 'users without access'
 
   context 'researcher deactivates the account' do
-    let(:user) { create(:user, :predefined_participant, active: true) }
-
     it 'activates the user' do
       expect { request }.to change { user.reload.active }.to(false)
     end
   end
 
   context 'when intervention has collaborators' do
-    let!(:intervention) { create(:intervention, :with_collaborators, user: researcher) }
+    let!(:intervention) { create(:intervention, :with_predefined_participants, :with_collaborators, user: researcher) }
 
     it 'no editing mode' do
       request
