d088380e1c038e10fde168972923a97e4fe10ed1
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-12 10:23:14 +0200

    remove commented out code

--- app/serializers/v1/predefined_participant_serializer.rb
+++ app/serializers/v1/predefined_participant_serializer.rb
@@ -7,22 +7,6 @@ class V1::PredefinedParticipantSerializer < V1Serializer
     object.phone.as_json(only: %i[iso prefix number confirmed])
   end
 
-  # attribute :slug do |object|
-  #   object.predefined_user_parameter.slug
-  # end
-  #
-  # attribute :auto_invitation do |object|
-  #   object.predefined_user_parameter.auto_invitation
-  # end
-  #
-  # attribute :invitation_sent_at do |object|
-  #   object.predefined_user_parameter.invitation_sent_at
-  # end
-  #
-  # attribute :health_clinic_id do |object|
-  #   object.predefined_user_parameter.health_clinic_id
-  # end
-
   %i[slug auto_invitation invitation_sent_at health_clinic_id].each do |attr|
     attribute attr do |object|
       object.predefined_user_parameter.send(attr)
