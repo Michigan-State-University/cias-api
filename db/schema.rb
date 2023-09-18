4414c3df6cce501f1dde5c94394bcec9cd39b110
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-09-18 08:27:35 +0200

    restore db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_01_094411) do
+ActiveRecord::Schema.define(version: 2023_09_05_090300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
