347debf7829b5c699972f1cc4f7d11a1b3c9b8bc
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-27 10:43:24 +0200

    update the base mailer class to have optional wrapping with locale

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  around_action :with_locale, if: -> { params && params[:language].present? }
+
   default from: ENV['EMAIL_DEFAULT_FROM']
   layout 'mailer'
+
+  protected
+
+  def with_locale
+    I18n.with_locale(params[:language]) { yield }
+  end
 end
