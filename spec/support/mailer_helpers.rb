36f4a295e20a1ce3e558ee8b29d0f05707bedbdd
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-28 08:09:14 +0200

    create a mailer helper method

---
+++ spec/support/mailer_helpers.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+#
+module MailerHelpers
+  def expect_to_call_mailer(mailer, method, args: nil, params: nil, return_value: nil)
+    expect(ActionMailer::Parameterized::MessageDelivery).to receive(:new)
+            .with(mailer, method, params.presence || anything, *args.presence || any_args)
+            .and_return(return_value || anything)
+  end
+end
