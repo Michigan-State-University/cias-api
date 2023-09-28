3e3b1b9f37c78fdfdd513c1ffcbe8a8b29649b37
oskardybowski <oskar.dybowski@htdhealth.com>
2023-09-28 08:23:00 +0200

    fix rubocop

--- spec/support/mailer_helpers.rb
+++ spec/support/mailer_helpers.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
-#
+
 module MailerHelpers
   def expect_to_call_mailer(mailer, method, args: nil, params: nil, return_value: nil)
+    # rubocop: disable RSpec/StubbedMock
     expect(ActionMailer::Parameterized::MessageDelivery).to receive(:new)
             .with(mailer, method, params.presence || anything, *args.presence || any_args)
             .and_return(return_value || anything)
+    # rubocop: enable RSpec/StubbedMock
   end
 end
