20dce3bb90bf469683afc990d42fe5e07071fc29
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-10-11 11:36:54 +0200

    WIP

--- app/services/v1/henry_ford/verify_service.rb
+++ app/services/v1/henry_ford/verify_service.rb
@@ -17,7 +17,8 @@ class V1::HenryFord::VerifyService
   attr_accessor :resource
 
   def call
-    @patient = Api::EpicOnFhir::PatientVerification.call(first_name, last_name, parsed_dob, phone_number, phone_type, zip_code)
+    require 'pry'; binding.pry
+    @patient = Api::EpicOnFhir::PatientVerification.call(first_name, last_name, parsed_dob, phone_number, phone_type, zip_code, mrn)
     @appointments = Api::EpicOnFhir::Appointments.call(epic_patient_id)
 
     create_or_find_resource!
