0dbd181e36d5ec188e481460a40c47b4f8714121
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-10-11 09:17:21 +0200

    delete all SMSes for provided number

--- app/services/v1/sms/replay.rb
+++ app/services/v1/sms/replay.rb
@@ -14,6 +14,9 @@ class V1::Sms::Replay
 
   def call
     return help_message unless message.casecmp('STOP').zero?
+
+    delete_messaged_for(from_number)
+    stop_message
   end
 
   attr_reader :from_number, :to_number, :message
@@ -26,7 +29,21 @@ class V1::Sms::Replay
     end
   end
 
+  def stop_message
+    response.message do |message|
+      message.body(I18n.t('sms_replay.stop'))
+    end
+  end
+
   def response
     @response ||= Twilio::TwiML::MessagingResponse.new
+  end
+
+  def delete_messaged_for(number)
+    queue = Sidekiq::ScheduledSet.new
+    queue.each do |job|
+      job_args = job.first.args
+      job.delete if (job_args['job_class'] == 'SmsPlans::SendSmsJob' && job_args['arguments'].first.eql?(number))
+    end
   end
 end
