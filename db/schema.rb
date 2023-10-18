a45cdef6ef86c185e2b29265c021b170a9fd3617
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-10-18 11:18:41 +0200

    add external name to clinic locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_12_092639) do
+ActiveRecord::Schema.define(version: 2023_10_18_071455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(version: 2023_10_12_092639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "external_id"
+    t.string "external_name"
     t.index ["external_id"], name: "index_clinic_locations_on_external_id", unique: true
   end
 
