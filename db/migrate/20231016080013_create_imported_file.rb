785ac9c824cbc9fd8702e40a486c4f020ea8425c
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-11-03 08:32:49 +0100

    undo

--- db/migrate/20231016080013_create_imported_file.rb
+++ db/migrate/20231016080013_create_imported_file.rb
@@ -1,6 +1,6 @@
 class CreateImportedFile < ActiveRecord::Migration[6.1]
   def change
-    create_table :import_files, id: :uuid, default: 'uuid_generate_v4()', null: false do |t|
+    create_table :imported_files, id: :uuid, default: 'uuid_generate_v4()', null: false do |t|
 
       t.timestamps
     end
