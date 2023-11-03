43d3564beb120638cca8091cbd77f01b9b10469f
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-11-03 08:27:44 +0100

    comment some part to run rollback on Heroku

--- db/migrate/20231018071455_add_external_name_to_clinic_location.rb
+++ db/migrate/20231018071455_add_external_name_to_clinic_location.rb
@@ -1,6 +1,6 @@
 class AddExternalNameToClinicLocation < ActiveRecord::Migration[6.1]
   def change
     add_column :clinic_locations, :external_name, :string
-    Rake::Task['one_time_use:add_external_name_to_clinic_locations'].invoke
+    # Rake::Task['one_time_use:add_external_name_to_clinic_locations'].invoke
   end
 end
