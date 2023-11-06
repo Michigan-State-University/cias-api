6594d00513540805e9771c94b93b4e600dd3451b
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2023-11-06 14:20:34 +0100

    [CIAS30-3754] ShortLink should move participant to active session (#193)
    * [CIAS30-3792] return information even not logged users (#194)

--- spec/requests/v1/interventions/short_links/verify_spec.rb
+++ spec/requests/v1/interventions/short_links/verify_spec.rb
@@ -2,14 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe 'GET /v1/verify_short_link', type: :request do
+RSpec.describe 'POST /v1/short_links/verify', type: :request do
   let(:participant) { create(:user, :confirmed, :participant) }
   let(:researcher) { create(:user, :confirmed, :researcher) }
   let(:intervention) { create(:intervention, :published, user: researcher) }
   let!(:short_link) { create(:short_link, linkable: intervention, name: 'short-link') }
-  let(:params) { { name: 'short-link' } }
+  let(:params) { { slug: 'short-link' } }
+  let(:user) { participant }
   let(:request) do
-    get v1_verify_short_links_path, params: params, headers: participant.create_new_auth_token
+    post v1_verify_short_links_path, params: params, headers: user.create_new_auth_token
   end
 
   context 'sequential intervention with sessions' do
@@ -24,10 +25,103 @@ RSpec.describe 'GET /v1/verify_short_link', type: :request do
     it {
       expect(json_response['data'].symbolize_keys).to include({
                                                                 intervention_id: intervention.id,
+                                                                session_id: session.id,
                                                                 health_clinic_id: nil,
-                                                                type: 'Intervention',
-                                                                first_session_id: session.id
+                                                                multiple_fill_session_available: false,
+                                                                user_intervention_id: nil
                                                               })
+    }
+  end
+
+  context 'without token' do
+    let(:request) do
+      post v1_verify_short_links_path, params: params
+    end
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:ok) }
+  end
+
+  context 'sequential intervention with open sessions' do
+    let!(:session) { create(:session, intervention: intervention) }
+    let(:user_intervention) { create(:user_intervention, user: participant, intervention: intervention) }
+    let!(:user_session) { create(:user_session, session: session, user: participant, user_intervention: user_intervention) }
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:ok) }
+
+    it {
+      expect(json_response['data'].symbolize_keys).to include({
+                                                                intervention_id: intervention.id,
+                                                                session_id: session.id,
+                                                                health_clinic_id: nil,
+                                                                multiple_fill_session_available: false,
+                                                                user_intervention_id: user_intervention.id
+                                                              })
+    }
+  end
+
+  context 'when intervention is draft' do
+    let(:intervention) { create(:intervention, user: researcher) }
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:bad_request) }
+
+    it {
+      expect(json_response).to include({ 'message' => 'Invention is not available', 'details' => { 'reason' => 'INTERVENTION_DRAFT' } })
+    }
+  end
+
+  context 'when intervention is archived/closed' do
+    let(:intervention) { create(:intervention, :closed, user: researcher) }
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:bad_request) }
+
+    it {
+      expect(json_response).to include({ 'message' => 'Invention is not available', 'details' => { 'reason' => 'INTERVENTION_CLOSED' } })
+    }
+  end
+
+  context 'quest without access' do
+    let(:user) { create(:user, :guest, :confirmed) }
+    let(:intervention) { create(:intervention, :published, user: researcher, shared_to: 'registered') }
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:unauthorized) }
+
+    it {
+      expect(json_response).to include({ 'message' => 'Interventions can only be completed by a registered user',
+                                         'details' => { 'reason' => 'ONLY_REGISTERED' } })
+    }
+  end
+
+  context 'participant without access' do
+    let(:intervention) { create(:intervention, :published, user: researcher, shared_to: 'invited') }
+
+    before do
+      request
+    end
+
+    it { expect(response).to have_http_status(:forbidden) }
+
+    it {
+      expect(json_response).to include({ 'message' => 'Interventions can only be completed by a invited user', 'details' => { 'reason' => 'ONLY_INVITED' } })
     }
   end
 
@@ -42,8 +136,9 @@ RSpec.describe 'GET /v1/verify_short_link', type: :request do
       expect(json_response['data'].symbolize_keys).to include({
                                                                 intervention_id: intervention.id,
                                                                 health_clinic_id: nil,
-                                                                type: 'Intervention',
-                                                                first_session_id: nil
+                                                                session_id: nil,
+                                                                multiple_fill_session_available: false,
+                                                                user_intervention_id: nil
                                                               })
     }
   end
@@ -62,8 +157,9 @@ RSpec.describe 'GET /v1/verify_short_link', type: :request do
       expect(json_response['data'].symbolize_keys).to include({
                                                                 intervention_id: intervention.id,
                                                                 health_clinic_id: nil,
-                                                                type: 'Intervention::FlexibleOrder',
-                                                                first_session_id: nil
+                                                                session_id: nil,
+                                                                multiple_fill_session_available: false,
+                                                                user_intervention_id: nil
                                                               })
     }
   end
