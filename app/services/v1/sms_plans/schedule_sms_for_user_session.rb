c1cde03ca279debf56f5b00e62bc80df6ae4f613
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2023-11-29 09:10:53 +0100

    add missing iclude

--- app/services/v1/sms_plans/schedule_sms_for_user_session.rb
+++ app/services/v1/sms_plans/schedule_sms_for_user_session.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class V1::SmsPlans::ScheduleSmsForUserSession
+  include Rails.application.routes.url_helpers
   include ::SmsHelper
 
   def self.call(user_session)
