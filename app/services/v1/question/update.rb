70c4e6e4130f892c39bed1eb5e08cc57bbf6d062
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2024-01-12 14:20:00 +0100

    maek variables incase-sensitive

--- app/services/v1/question/update.rb
+++ app/services/v1/question/update.rb
@@ -12,6 +12,7 @@ class V1::Question::Update
 
   def call
     raise ActiveRecord::RecordNotSaved, I18n.t('question.error.published_intervention') if question.session.published?
+    raise ActiveRecord::RecordNotSaved, I18n.t('question.error.not_uniq_variable') if new_variable_is_taken?(new_variables)
 
     question.assign_attributes(question_params.except(:type))
     question.execute_narrator
@@ -23,4 +24,25 @@ class V1::Question::Update
 
   attr_reader :question_params
   attr_accessor :question
+
+  def new_variables
+    return [] if question.is_a?(Question::TlfbQuestion)
+    return question_params&.dig(:body, :data)&.map { |row| row.dig(:variable, :name)&.downcase } if question.is_a?(Question::Multiple)
+
+    if question.is_a?(Question::Grid)
+      return question_params&.dig(:body, :data)&.first&.dig(:payload, :rows)&.map do |row|
+               row.dig(:variable, :name).downcase
+             end&.reject(&:empty?)
+    end
+
+    [question_params.dig(:body, :variable, :name)&.downcase]
+  end
+
+  def new_variable_is_taken?(new_variables)
+    return if new_variables.blank?
+
+    used_variables = question.session.fetch_variables.pluck(:variables).flatten.map(&:downcase)
+
+    used_variables.any? { |variable| new_variables.include?(variable) }
+  end
 end
