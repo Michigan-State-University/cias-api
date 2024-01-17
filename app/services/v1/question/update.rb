d2fb9568ef3c94308c1d4379ec4ced856dc18059
Mikołaj Imbor <mikolaj.imbor@htdevelopers.com>
2024-01-17 15:40:57 +0100

    CIAS30-3880 Add job usage to question update action

--- app/services/v1/question/update.rb
+++ app/services/v1/question/update.rb
@@ -13,9 +13,11 @@ class V1::Question::Update
   def call
     raise ActiveRecord::RecordNotSaved, I18n.t('question.error.published_intervention') if question.session.published?
 
+    previous_var = question.body['variable']['name']
     question.assign_attributes(question_params.except(:type))
     question.execute_narrator
     question.save!
+    adjust_reflections(previous_var)
     question
   end
 
@@ -23,4 +25,10 @@ class V1::Question::Update
 
   attr_reader :question_params
   attr_accessor :question
+
+  def adjust_reflections(previous_variable)
+    return if previous_variable == include?(question.body['variable']['name'])
+
+    UpdateJobs::AdjustQuestionReflections.perform_later(question, previous_variable)
+  end
 end
