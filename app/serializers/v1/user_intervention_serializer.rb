ce26f3f66fcd13da1b76dc407e7d578c0e3e93f3
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2024-01-22 19:36:33 +0100

    rubocop

--- app/serializers/v1/user_intervention_serializer.rb
+++ app/serializers/v1/user_intervention_serializer.rb
@@ -17,12 +17,12 @@ class V1::UserInterventionSerializer < V1Serializer
   end
 
   attribute :blocked do |object|
-   object.intervention.shared_to_invited? &&
+    object.intervention.shared_to_invited? &&
       object.intervention.intervention_accesses
             .pluck(:email)
             .map(&:downcase)
             .exclude?(object.user.email) &&
-        object.user.roles.exclude?('predefined_participant')
+      object.user.roles.exclude?('predefined_participant')
   end
 
   attribute :intervention do |object|
