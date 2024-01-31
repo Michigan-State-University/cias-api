5bbf07058033b1b9e4f9638528a6d66c11bd81a2
Mikołaj Imbor <mikolaj.imbor@htdevelopers.com>
2024-01-31 16:19:42 +0100

    CIAS30-3880 Adjust query in reflectable questions to avoid check in the loop

--- app/jobs/update_jobs/adjust_question_reflections.rb
+++ app/jobs/update_jobs/adjust_question_reflections.rb
@@ -2,13 +2,11 @@
 
 class UpdateJobs::AdjustQuestionReflections < CloneJob
   def perform(question, prev_variable_name)
-    reflectable_questions(question).each do |q|
+    reflectable_questions(question, prev_variable_name).each do |q|
       q.narrator['blocks'].each do |block|
         next if block['reflections'].nil?
 
         block['reflections'].each do |reflection|
-          next unless reflection['variable'] == prev_variable_name
-
           reflection['variable'] = question.body['variable']['name']
         end
 
@@ -17,11 +15,21 @@ class UpdateJobs::AdjustQuestionReflections < CloneJob
     end
   end
 
-  def reflectable_questions(question)
+  def reflectable_questions(question, variable_name)
     Question
        .within_intervention(question.session.intervention_id)
        .where.not(id: question.id)
        .joins("CROSS JOIN jsonb_array_elements(narrator->'blocks') AS narrator_block")
        .where('narrator_block @> ?', { type: 'Reflection', question_id: question.id }.to_json)
+       .where("narrator_block->>'reflections' IS NOT NULL")
+       .where(
+         "EXISTS (
+           SELECT 1
+           FROM jsonb_array_elements(narrator_block->'reflections')
+           AS reflection
+           WHERE reflection->>'variable' = ?
+         )",
+         variable_name
+       )
   end
 end
