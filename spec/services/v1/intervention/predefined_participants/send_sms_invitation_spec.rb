1da005e42f9d565efe4ca9442ab8a9338f210f14
mimbor <86961364+mimbor@users.noreply.github.com>
2024-02-12 16:53:53 +0100

    [CIAS30-3904] Add arabic and spanish translations to PDP messages (#243)
    * CIAS30-3904 Add arabic and spanish translations to PDP messages
    
    * CIAS30-3904 Add missing translations
    
    * CIAS30-3904 Add specs for sms with custom language
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6
    
    * CIAS30-3911 Update Ruby from 2.7.2 to 3.0.6

--- spec/services/v1/intervention/predefined_participants/send_sms_invitation_spec.rb
+++ spec/services/v1/intervention/predefined_participants/send_sms_invitation_spec.rb
@@ -30,6 +30,17 @@ RSpec.describe V1::Intervention::PredefinedParticipants::SendSmsInvitation do
       expect(message.body).to include("#{ENV['WEB_URL']}/usr/#{user.predefined_user_parameter.slug}")
     end
 
+    context 'with custom intervention language' do
+      let!(:google_language) { create(:google_language, language_code: 'es') }
+      let(:intervention) { create(:intervention, :with_predefined_participants, google_language: google_language) }
+
+      it 'has expected message body' do
+        subject
+        message = Message.order(:created_at).last
+        expect(message.body).to include('¡Hola! Por favor, haga clic en el enlace de abajo para comenzar o continuar su progreso en')
+      end
+    end
+
     context 'when the user is deactivated' do
       before do
         user.update!(active: false)
