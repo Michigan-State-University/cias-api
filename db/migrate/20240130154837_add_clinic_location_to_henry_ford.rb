79412121b85537a754d5a5dcd57503f376fe5c5e
Mikołaj Imbor <mikolaj.imbor@htdevelopers.com>
2024-02-22 09:17:20 +0100

    CIAS30-3914 Add new clinics and fix Bad Request error from patient verification

--- db/migrate/20240130154837_add_clinic_location_to_henry_ford.rb
+++ db/migrate/20240130154837_add_clinic_location_to_henry_ford.rb
@@ -8,11 +8,27 @@ class AddClinicLocationToHenryFord < ActiveRecord::Migration[6.1]
   end
 
   def add_new_clinic_location_to_henry_ford!
-    AuxiliaryClinicLocation.find_or_create_by!(
-      department: '',
-      name: 'HAMTRAMCK PEDIATRICS',
-      external_id: '1010210018',
-      external_name: 'HAMTRAMCK PEDIATRICS',
-    )
+    [
+      {
+        department: '',
+        name: 'SBH LINCOLN PARK MIDDLE SCHOOL HEALTH CTR',
+        external_id: '1011430001',
+        external_name: 'Henry Ford School Based Health Center -  Lincoln Park Middle School',
+      },
+      {
+        department: '',
+        name: 'SBH MUMFORD HEALTH CTR',
+        external_id: '1010010181',
+        external_name: 'Henry Ford School Based Health Center -  Samuel C. Mumford High School',
+      },
+      {
+        department: '',
+        name: 'SBH EARHART HEALTH CTR',
+        external_id: '1010010222',
+        external_name: 'Henry Ford School Based Health Center -  Amelia Earhart Elementary / Middle School',
+      }
+    ].each do |clinic_location_params|
+      AuxiliaryClinicLocation.find_or_create_by!(clinic_location_params)
+    end
   end
 end
