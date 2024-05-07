e169f799f460f0466cbc44eddb66f90b6ac923bb
Mikołaj Imbor <mikolaj.imbor@htdevelopers.com>
2024-05-07 16:44:03 +0200

    CIAS30-Upgrade-ruby-to-3-1-4 Revert changes in performance tests

--- spec/performance/sessions_spec.rb
+++ spec/performance/sessions_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe 'Performance', type: :request do
 
     it 'performs index in correct time' do
       expect { get v1_intervention_sessions_path(intervention.id), headers: headers }
-        .to perform_under(0.25).sample(10)
+        .to perform_under(0.2).sample(10)
     end
 
     it 'performs show in correct time' do
       expect { get v1_intervention_session_path(intervention_id: intervention.id, id: session.id), headers: headers }
-        .to perform_under(0.25).sample(10)
+        .to perform_under(0.2).sample(10)
     end
   end
 end
