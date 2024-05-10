5c57b942d6cf7293f3ea18fa2f8c1186be973582
krzepecki <50991414@users.noreply.github.com>
2024-05-10 22:29:01 +0200

    CIAS30-3782: code cleanup

--- app/models/user_session/sms.rb
+++ app/models/user_session/sms.rb
@@ -3,44 +3,8 @@
 class UserSession::Sms < UserSession
   delegate :first_question, :autofinish_enabled, :autofinish_delay, :questions, to: :session
 
-  def on_answer
-    cancel_timeout_job
-    return unless autofinish_enabled
-
-    if any_question_run_timeout?
-      set_timeout_job if timeout_job_id.present? || last_answer&.question&.settings&.dig('start_autofinish_timer')
-    else
-      set_timeout_job
-    end
-  end
-
-  def cancel_timeout_job
-    return if timeout_job_id.nil?
-
-    UserSessionTimeoutJob.cancel_by(provider_job_id: timeout_job_id)
-
-    update(timeout_job_id: nil)
-  end
-
   def last_answer
     answers.confirmed.unscope(:order).order(:updated_at).last
-  end
-
-  def finish(send_email: true)
-    return if finished_at
-
-    cancel_timeout_job
-    update(finished_at: DateTime.current)
-    delete_alternative_answers
-    reload
-
-    V1::SmsPlans::ScheduleSmsForUserSession.call(self)
-    V1::UserSessionScheduleService.new(self).schedule if send_email
-    V1::ChartStatistics::CreateForUserSession.call(self)
-
-    AfterFinishUserSessionJob.perform_later(id, session.intervention)
-
-    update_user_intervention(session_is_finished: true)
   end
 
   def find_current_question
@@ -50,21 +14,5 @@ class UserSession::Sms < UserSession
       .where.not(answer: nil)
       .order('question_groups.position ASC, questions.position ASC')
       .first
-  end
-
-  private
-
-  def any_question_run_timeout?
-    questions.where("settings @> '{\"start_autofinish_timer\": true}'").any?
-  end
-
-  def set_timeout_job
-    timeout_job = UserSessionTimeoutJob.set(wait: autofinish_delay.minutes).perform_later(id)
-    cancel_timeout_job
-    update(last_answer_at: DateTime.current, timeout_job_id: timeout_job.provider_job_id)
-  end
-
-  def delete_alternative_answers
-    answers.where(draft: true, alternative_branch: true).destroy_all
   end
 end
