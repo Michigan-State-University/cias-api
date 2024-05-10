3e866ff6ef7b06515154de882c3acc6661c50a61
krzepecki <50991414@users.noreply.github.com>
2024-05-10 23:39:38 +0200

    CIAS30-3782: cleanup fix4

--- app/models/session/sms.rb
+++ app/models/session/sms.rb
@@ -10,8 +10,8 @@ class Session::Sms < Session
   has_many :questions, through: :question_groups
   has_many :answers, dependent: :destroy, through: :questions
 
-  validates :sms_code, presence: true, if: -> { published? }
-  validates :sms_code, uniqueness: true, length: { minimum: SMS_CODE_MIN_LENGTH }
+  validates :sms_code, presence: true, length: { minimum: SMS_CODE_MIN_LENGTH }, if: -> { published? }
+  validates :sms_code, uniqueness: true
 
   attribute :settings, :json, default: { 'narrator' => { 'voice' => false, 'animation' => false } }
 
