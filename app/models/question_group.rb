9e7b1bcf5bbdcdd3076af1c16479ecc85d829eef
krzepecki <50991414@users.noreply.github.com>
2024-05-13 17:51:54 +0200

    CIAS30-3782: rubocop

--- app/models/question_group.rb
+++ app/models/question_group.rb
@@ -18,10 +18,10 @@ class QuestionGroup < ApplicationRecord
   delegate :ability_to_update_for?, to: :session
 
   attribute :sms_schedule, :jsonb, default: {
-                                               "time": {},
-                                               "day_of_period": [],
-                                               "questions_per_day": 1
-                                             }
+    time: {},
+    day_of_period: [],
+    questions_per_day: 1
+  }
   validates :sms_schedule,
             json: { schema: -> { Rails.root.join('db/schema/_common/sms_schedule.json').to_s },
                     message: ->(err) { err } }, if: -> { session&.type&.match?('Session::Sms') },
