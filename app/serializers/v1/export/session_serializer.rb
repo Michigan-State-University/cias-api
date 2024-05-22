8733480e5d6442ecf0ad2f531bdb5032b3ad59f8
krzepecki <50991414@users.noreply.github.com>
2024-05-22 14:45:03 +0200

    CIAS30-3782: fixed import/export intervention

--- app/serializers/v1/export/session_serializer.rb
+++ app/serializers/v1/export/session_serializer.rb
@@ -10,15 +10,15 @@ class V1::Export::SessionSerializer < ActiveModel::Serializer
   has_many :report_templates, serializer: V1::Export::ReportTemplateSerializer
 
   attribute :voice_type do
-    object.google_tts_voice.voice_type
+    object.google_tts_voice&.voice_type
   end
 
   attribute :voice_label do
-    object.google_tts_voice.voice_label
+    object.google_tts_voice&.voice_label
   end
 
   attribute :language_code do
-    object.google_tts_voice.language_code
+    object.google_tts_voice&.language_code
   end
 
   attribute :relations_data do
