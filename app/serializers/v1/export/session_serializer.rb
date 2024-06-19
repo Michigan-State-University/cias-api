20260299dc3bf6bd0b1c986d8391f774c2b39b0d
krzepecki <50991414@users.noreply.github.com>
2024-06-19 10:30:27 +0200

    CIAS30-3782: fixed bugs found in testing phase

--- app/serializers/v1/export/session_serializer.rb
+++ app/serializers/v1/export/session_serializer.rb
@@ -3,7 +3,8 @@
 class V1::Export::SessionSerializer < ActiveModel::Serializer
   include ExportHelper
   attributes :settings, :position, :name, :schedule, :schedule_payload, :schedule_at, :formulas, :variable,
-             :days_after_date_variable_name, :type, :original_text, :estimated_time, :multiple_fill, :body, :current_narrator
+             :days_after_date_variable_name, :type, :original_text, :estimated_time, :multiple_fill, :body, :current_narrator,
+             :welcome_message, :default_response
 
   has_many :question_groups, serializer: V1::Export::QuestionGroupSerializer
   has_many :sms_plans, serializer: V1::Export::SmsPlanSerializer
