fa8f100679552b606d5063ad725b3b4993b90e8e
krzepecki <50991414@users.noreply.github.com>
2025-01-15 11:22:42 +0100

    CIAS30-3948: fix

--- app/models/google_language.rb
+++ app/models/google_language.rb
@@ -5,7 +5,7 @@ class GoogleLanguage < ApplicationRecord
   belongs_to :google_tts_language, inverse_of: :google_tts_voices, optional: true
 
   def default_google_tts_voice
-    return google_tts_language.google_tts_voices.standard_voices.first if google_tts_language.present? && google_tts_voices.standard_voices.any?
+    return google_tts_language.google_tts_voices.standard_voices.first if google_tts_language.present? && google_tts_language.google_tts_voices.standard_voices.any?
 
     first_word = language_name.split.first
     GoogleTtsLanguage.where('language_name like?', "%#{first_word}%")&.first&.google_tts_voices&.standard_voices&.first
