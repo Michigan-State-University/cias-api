cfd1adf11105cba3b6ea1d14554328f8ce3f7927
krzepecki <50991414@users.noreply.github.com>
2025-03-24 15:06:42 +0100

    PDP_FIX: add proper pdp removal scoping

--- app/jobs/data_clear_jobs/clear_user_data.rb
+++ app/jobs/data_clear_jobs/clear_user_data.rb
@@ -10,7 +10,7 @@ class DataClearJobs::ClearUserData < ApplicationJob
       intervention.user_interventions.destroy_all
       intervention.conversations.destroy_all
       intervention.conversations_transcript.destroy
-      delete_temporary_users_without_any_intervention!
+      delete_temporary_users_without_intervention!(intervention)
 
       intervention.sensitive_data_removed!
       create_notification!(intervention)
@@ -19,9 +19,15 @@ class DataClearJobs::ClearUserData < ApplicationJob
 
   private
 
-  def delete_temporary_users_without_any_intervention!
+  def delete_temporary_users_without_intervention!(intervention)
+    User.left_joins(:user_interventions, :predefined_user_parameter)
+        .where("CAST(roles AS TEXT[]) && ARRAY['predefined_participant']
+                AND user_interventions.id IS NULL
+                AND predefined_user_parameters.intervention_id = '#{intervention.id}'")
+        .destroy_all
+
     User.left_joins(:user_interventions)
-        .where("CAST(roles AS TEXT[]) && ARRAY['guest', 'predefined_participant'] AND user_interventions.id IS NULL")
+        .where("CAST(roles AS TEXT[]) && ARRAY['guest'] AND user_interventions.id IS NULL")
         .destroy_all
   end
 
