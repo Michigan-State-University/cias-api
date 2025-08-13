96a3d81a46b3851ae590a2ca2dc9012f82609c35
szymonklempert <szymon.klempert@gmail.com>
2025-08-13 15:46:53 +0200

    Rename skip_warning_screen to warning_screen_enabled in interventions and update related serializers and views

--- db/migrate/20250813103722_add_warning_screen_enabled_to_interventions.rb
+++ db/migrate/20250813103722_add_warning_screen_enabled_to_interventions.rb
@@ -1,5 +1,5 @@
 class AddWarningScreenEnabledToInterventions < ActiveRecord::Migration[7.2]
   def change
-    add_column :interventions, :warning_screen_enabled, :boolean
+    add_column :interventions, :warning_screen_enabled, :boolean, default: false, null: false
   end
 end
