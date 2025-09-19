e355f492633a802bbd6eb4bb1dbba4f580d66f9e
Mikołaj Imbor <mikolaj.imbor@htdevelopers.com>
2025-09-19 14:08:19 +0200

    CIAS30-3934 Adjust each to find_each

--- app/models/concerns/clone/sms_plan.rb
+++ app/models/concerns/clone/sms_plan.rb
@@ -15,7 +15,7 @@ class Clone::SmsPlan < Clone::Base
   private
 
   def create_sms_variants
-    source.variants.each do |variant|
+    source.variants.find_each do |variant|
       outcome.variants << SmsPlan::Variant.new(variant.slice(SmsPlan::Variant::ATTR_NAMES_TO_COPY))
     end
   end
@@ -23,13 +23,13 @@ class Clone::SmsPlan < Clone::Base
   def create_alert_phones
     return unless source.alert?
 
-    source.alert_phones.each do |alert_phone|
+    source.alert_phones.find_each do |alert_phone|
       outcome.alert_phones << AlertPhone.new(sms_plan: outcome, phone: alert_phone.phone)
     end
   end
 
   def create_sms_links
-    source.sms_links.each do |sms_link|
+    source.sms_links.find_each do |sms_link|
       outcome.sms_links << SmsLink.new(sms_plan: outcome, url: sms_link.url, link_type: sms_link.link_type, variable: sms_link.variable)
     end
   end
