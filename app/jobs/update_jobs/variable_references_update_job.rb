0acebfea1a3272abf54f42f90076605804d77ebb
szymonklempert <szymon.klempert@gmail.com>
2025-10-13 16:10:34 +0200

    refactor: Optimize base query methods with memoization for improved performance

--- app/jobs/update_jobs/variable_references_update_job.rb
+++ app/jobs/update_jobs/variable_references_update_job.rb
@@ -249,46 +249,54 @@ class UpdateJobs::VariableReferencesUpdateJob < CloneJob
   end
 
   def build_question_base_query(session, exclude_source_session)
-    if exclude_source_session
-      Question.joins(question_group: { session: :intervention })
-              .where(interventions: { id: session.intervention_id })
-              .where.not(question_groups: { session_id: session.id })
-    else
-      Question.joins(:question_group)
-              .where(question_groups: { session_id: session.id })
-    end
+    @question_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @question_base_query[key] ||= if exclude_source_session
+                                    Question.joins(question_group: { session: :intervention })
+                                            .where(interventions: { id: session.intervention_id })
+                                            .where.not(question_groups: { session_id: session.id })
+                                  else
+                                    Question.joins(:question_group)
+                                            .where(question_groups: { session_id: session.id })
+                                  end
   end
 
   def build_question_group_base_query(session, exclude_source_session)
-    if exclude_source_session
-      QuestionGroup.joins(session: :intervention)
-                   .where(interventions: { id: session.intervention_id })
-                   .where.not(session_id: session.id)
-                   .where.not(formulas: nil)
-    else
-      QuestionGroup.where(session_id: session.id)
-                   .where.not(formulas: nil)
-    end
+    @question_group_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @question_group_base_query[key] ||= if exclude_source_session
+                                          QuestionGroup.joins(session: :intervention)
+                                                      .where(interventions: { id: session.intervention_id })
+                                                      .where.not(session_id: session.id)
+                                                      .where.not(formulas: nil)
+                                        else
+                                          QuestionGroup.where(session_id: session.id)
+                                                      .where.not(formulas: nil)
+                                        end
   end
 
   def build_session_base_query(session, exclude_source_session)
-    if exclude_source_session
-      Session.where(intervention_id: session.intervention_id)
-             .where.not(id: session.id)
-    else
-      Session.where(id: session.id)
-    end
+    @session_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @session_base_query[key] ||= if exclude_source_session
+                                   Session.where(intervention_id: session.intervention_id)
+                                          .where.not(id: session.id)
+                                 else
+                                   Session.where(id: session.id)
+                                 end
   end
 
   def build_report_template_section_base_query(session, exclude_source_session)
-    if exclude_source_session
-      ReportTemplate::Section.joins(report_template: { session: :intervention })
-                             .where(interventions: { id: session.intervention_id })
-                             .where.not(sessions: { id: session.id })
-    else
-      ReportTemplate::Section.joins(report_template: :session)
-                             .where(sessions: { id: session.id })
-    end
+    @report_template_section_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @report_template_section_base_query[key] ||= if exclude_source_session
+                                                   ReportTemplate::Section.joins(report_template: { session: :intervention })
+                                                                         .where(interventions: { id: session.intervention_id })
+                                                                         .where.not(sessions: { id: session.id })
+                                                 else
+                                                   ReportTemplate::Section.joins(report_template: :session)
+                                                                         .where(sessions: { id: session.id })
+                                                 end
   end
 
   def sanitize_like_pattern(pattern)
