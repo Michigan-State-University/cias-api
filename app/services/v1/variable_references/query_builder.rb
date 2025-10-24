ded97600d32e4106e1ae9759042a8eb9f730e958
szymonklempert <szymon.klempert@gmail.com>
2025-10-24 12:45:27 +0200

    Add SqlBuilder and QueryBuilder modules for variable reference management

---
+++ app/services/v1/variable_references/query_builder.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module V1::VariableReferences::QueryBuilder
+  private
+
+  def build_question_base_query(session, exclude_source_session)
+    @question_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @question_base_query[key] ||= if exclude_source_session
+                                    Question.joins(question_group: { session: :intervention })
+                                            .where(interventions: { id: session.intervention_id })
+                                            .where.not(question_groups: { session_id: session.id })
+                                  else
+                                    Question.joins(:question_group)
+                                            .where(question_groups: { session_id: session.id })
+                                  end
+  end
+
+  def build_question_group_base_query(session, exclude_source_session)
+    @question_group_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @question_group_base_query[key] ||= if exclude_source_session
+                                          QuestionGroup.joins(session: :intervention)
+                                                      .where(interventions: { id: session.intervention_id })
+                                                      .where.not(session_id: session.id)
+                                                      .where.not(formulas: nil)
+                                        else
+                                          QuestionGroup.where(session_id: session.id)
+                                                      .where.not(formulas: nil)
+                                        end
+  end
+
+  def build_session_base_query(session, exclude_source_session)
+    @session_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @session_base_query[key] ||= if exclude_source_session
+                                   Session.where(intervention_id: session.intervention_id)
+                                          .where.not(id: session.id)
+                                 else
+                                   Session.where(id: session.id)
+                                 end
+  end
+
+  def build_report_template_section_base_query(session, exclude_source_session)
+    @report_template_section_base_query ||= {}
+    key = [session.id, exclude_source_session]
+    @report_template_section_base_query[key] ||= if exclude_source_session
+                                                   ReportTemplate::Section.joins(report_template: { session: :intervention })
+                                                                         .where(interventions: { id: session.intervention_id })
+                                                                         .where.not(sessions: { id: session.id })
+                                                 else
+                                                   ReportTemplate::Section.joins(report_template: :session)
+                                                                         .where(sessions: { id: session.id })
+                                                 end
+  end
+end
