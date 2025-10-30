4cde43a4b616bd78b7cdefb823c9eb5d5b2b9f3f
mimbor <mikolaj.imbor@htdevelopers.com>
2025-10-30 12:22:30 +0100

    CIAS30-4008 Remove unused param

--- app/controllers/v1/henry_ford/patient_details_controller.rb
+++ app/controllers/v1/henry_ford/patient_details_controller.rb
@@ -19,7 +19,7 @@ class V1::HenryFord::PatientDetailsController < V1Controller
   private
 
   def patient_detail_params
-    params.require(:hfhs_patient_data).permit(:id, :first_name, :last_name, :dob, :sex, :zip_code, :phone_number, :phone_type, :mrn, :hfhs_patient_detail_id)
+    params.require(:hfhs_patient_data).permit(:id, :first_name, :last_name, :dob, :sex, :zip_code, :phone_number, :phone_type, :mrn)
   end
 
   def verify_code_params
--- app/services/v1/henry_ford/verify_service.rb
+++ app/services/v1/henry_ford/verify_service.rb
@@ -22,9 +22,6 @@ class V1::HenryFord::VerifyService
     if patient_params[:id].present?
       @resource = HfhsPatientDetail.find(patient_params[:id])
       confirm_resource!
-    elsif patient_params[:hfhs_patient_detail_id].present?
-      @resource = HfhsPatientDetail.find(patient_params[:hfhs_patient_detail_id])
-      confirm_resource!
     else
       @patient = Api::EpicOnFhir::PatientVerification.call(first_name, last_name, parsed_dob, phone_number, phone_type, zip_code, mrn)
       create_or_find_resource!
