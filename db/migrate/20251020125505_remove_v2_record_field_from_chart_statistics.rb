badde672fce86648b87cba97ff8f837860f5e4d1
mimbor <mikolaj.imbor@htdevelopers.com>
2025-10-31 11:40:06 +0100

    CIAS30-4028 Remove unsafe deletion from migration

--- db/migrate/20251020125505_remove_v2_record_field_from_chart_statistics.rb
+++ db/migrate/20251020125505_remove_v2_record_field_from_chart_statistics.rb
@@ -2,19 +2,6 @@
 
 class RemoveV2RecordFieldFromChartStatistics < ActiveRecord::Migration[7.2]
   def change
-    remove_legacy_records
     remove_column :chart_statistics, :v2_record
-  end
-
-  class AuxiliaryChartStatistic < ApplicationRecord
-    self.table_name = 'chart_statistics'
-  end
-
-  private
-
-  def remove_legacy_records
-    if AuxiliaryChartStatistic.where(v2_record: true).any?
-      AuxiliaryChartStatistic.where(v2_record: false, user_session_id: nil).delete_all
-    end
   end
 end
