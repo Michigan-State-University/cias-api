03707752d05a1f123daccd9d4d4ff78d24e0cfbb
szymonklempert <szymon.klempert@gmail.com>
2025-11-28 09:39:38 +0100

    Implement SMS plan formula updates and add corresponding tests

--- app/services/v1/variable_references/base_service.rb
+++ app/services/v1/variable_references/base_service.rb
@@ -59,4 +59,10 @@ class V1::VariableReferences::BaseService
     update_sql = build_text_formula_update_sql('report_template_sections', 'formula', old_var, new_var, base_query)
     ActiveRecord::Base.connection.execute(update_sql)
   end
+
+  def update_sms_plan_formulas_scoped(session, old_var, new_var, exclude_source_session: false)
+    base_query = build_sms_plan_base_query(session, exclude_source_session)
+    update_sql = build_text_formula_update_sql('sms_plans', 'formula', old_var, new_var, base_query)
+    ActiveRecord::Base.connection.execute(update_sql)
+  end
 end
