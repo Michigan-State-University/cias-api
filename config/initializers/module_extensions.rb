b911f3db604d52ad09d10e9c34a6b66cc5287263
mimbor <mikolaj.imbor@htdevelopers.com>
2025-12-22 17:16:15 +0100

    CIAS30-4105 Remove legacy configuration

--- config/initializers/module_extensions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'action_cable/engine'
-require 'action_cable/subscription_adapter/redis'
-require_relative '../../app/channels/concerns/redis_adapter_overrides'
-
-ActionCable::SubscriptionAdapter::Redis.include(RedisAdapterOverrides)
