9d78fd0859d7db0873bbec3064d5147e1767331f
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2025-12-29 15:23:31 +0100

    modify cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('WEB_URL', nil)
+    origins '*'
     resource '*',
              headers: :any,
              credentials: true,
