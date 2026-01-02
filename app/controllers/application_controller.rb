e9c1eece28ef627ac2669c990e1c82b5868b7432
mimbor <mikolaj.imbor@htdevelopers.com>
2026-01-02 12:49:27 +0100

    CIAS30-4050 Handle special header from E2E tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :require_custom_header, if: :e2e_origin?
   before_action :user_params, if: :devise_controller?
   include Log
 
@@ -12,5 +13,18 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name time_zone terms])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[email])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[first_name last_name phone])
+  end
+
+  def require_custom_header
+    return if ActiveSupport::SecurityUtils.secure_compare(
+      request.headers['End-To-End-Secure-Token'],
+      ENV.fetch('CI_E2E_TOKEN', '')
+    )
+
+    render json: { error: 'Forbidden' }, status: :forbidden
+  end
+
+  def e2e_origin?
+    request.headers['Origin'] == ENV.fetch('CI_E2E_WEB_URL', '')
   end
 end
