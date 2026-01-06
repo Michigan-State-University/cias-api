7f8471e5f3ed8dfa6e0de08819d42ee039c6ce49
Wiktoria Rozanska <wiktoria.rozanska@htdevelopers.com>
2026-01-06 09:00:37 +0100

    cover Question::Grid in the migration

--- db/migrate/20260102135619_assign_id_to_answer_in_question_body.rb
+++ db/migrate/20260102135619_assign_id_to_answer_in_question_body.rb
@@ -17,6 +17,53 @@ class AssignIdToAnswerInQuestionBody < ActiveRecord::Migration[7.2]
       WHERE type IN ('Question::ThirdParty', 'Question::Single', 'Question::Multiple')
     SQL
     ActiveRecord::Base.connection.exec_query(query)
+
+    p '-- add new properties to body in grid question -> answer id'
+    grid_query = <<~SQL.squish
+      UPDATE questions
+      SET body = jsonb_set(
+        body,
+        '{data}',
+        (
+          SELECT jsonb_agg(
+            jsonb_set(
+              jsonb_set(
+                element,
+                '{id}',
+                to_jsonb(uuid_generate_v4()),
+                true
+              ),
+              '{payload}',
+              jsonb_set(
+                jsonb_set(
+                  element->'payload',
+                  '{rows}',
+                  (
+                    SELECT jsonb_agg(
+                      jsonb_set(row_el, '{id}', to_jsonb(uuid_generate_v4()), true)
+                    )
+                    FROM jsonb_array_elements(element->'payload'->'rows') AS row_el
+                  ),
+                  true
+                ),
+                '{columns}',
+                (
+                  SELECT jsonb_agg(
+                    jsonb_set(col_el, '{id}', to_jsonb(uuid_generate_v4()), true)
+                  )
+                  FROM jsonb_array_elements(element->'payload'->'columns') AS col_el
+                ),
+                true
+              ),
+              true
+            )
+          )
+          FROM jsonb_array_elements(body->'data') AS element
+        )
+      )
+      WHERE type IN ('Question::Grid');
+    SQL
+    ActiveRecord::Base.connection.exec_query(grid_query)
   end
 
   def down
