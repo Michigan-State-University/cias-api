d5c4fdeed558c45389210c51d0c4c54a8a250e86
szymonklempert <szymon.klempert@gmail.com>
2026-04-27 13:31:28 +0200

    Refactor position method for clarity and update response attributes in create_spec

--- app/controllers/concerns/resource/position.rb
+++ app/controllers/concerns/resource/position.rb
@@ -3,17 +3,15 @@
 module Resource::Position
   extend ActiveSupport::Concern
 
-  included do
-    def position
-      authorize! :update, model_constant
+  def position
+    authorize! :update, model_constant
 
-      SqlQuery.new(
-        'resource/position_bulk_update',
-        values: position_params[:position],
-        table: controller_name
-      ).execute
-      render json: serialized_response(response_scope["#{controller_name.classify.underscore}s"])
-    end
+    SqlQuery.new(
+      'resource/position_bulk_update',
+      values: position_params[:position],
+      table: controller_name
+    ).execute
+    render json: serialized_response(response_scope["#{controller_name.classify.underscore}s"])
   end
 
   private
